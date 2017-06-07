88d159118b2a4b3166393480d78b1406e7dab017
mm0nr0e <monroe.mirand@gmail.com>
2017-06-07 09:42:05 -0700

    finished hackhour challenge

--- challenges/fizzbuzz.js
+++ challenges/fizzbuzz.js
@@ -17,7 +17,18 @@
 //                     16 ]
 
 function fizzbuzz(num) {
-
+    var output = [];
+    for (var i = 1; i <= num; i++){
+        var str = '';
+        if (i % 3 === 0) str += 'fizz';
+        if (i % 5 === 0) str += 'buzz';
+        if (str.length > 0) {
+            output.push(str);
+        } else {
+            output.push(i)
+        }
+    }
+    return output;
 }
 
 module.exports = fizzbuzz;
