1b520e180f858952c0116632afc0935b7386b46d
Mustafa Khan <mustafakhansmt@gmail.com>
2017-06-07 09:36:40 -0700

    Done

--- challenges/fizzbuzz.js
+++ challenges/fizzbuzz.js
@@ -17,7 +17,18 @@
 //                     16 ]
 
 function fizzbuzz(num) {
-
+    var arr = []
+    for (let x = 0; x <= num; x++) {
+        var str = ""
+        if (x % 3 === 0) {
+            str += "fizz"
+        }
+        if (x % 5 === 0) {
+            str += "buzz"
+        }
+        arr.push(str || x)
+    }
+    return arr;
 }
-
+console.log(fizzbuzz(16))
 module.exports = fizzbuzz;
