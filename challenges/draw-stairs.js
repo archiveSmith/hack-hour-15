72a489a698daa359d6f3d2822e3af66205e1f538
Brett Beekley <brettbeekley@gmail.com>
2017-06-09 09:50:19 -0700

    Finished day 5 hack hour

--- challenges/draw-stairs.js
+++ challenges/draw-stairs.js
@@ -14,8 +14,33 @@
 */
 
 function drawStairs(n) {
-
+    // Check for valid input
+    if (typeof n !== 'number'
+       || n < 1
+       || n > 100
+       || n % 1 != 0) return 'Invalid number';
+    
+    // Print each line of stairs
+    for (let i = 0; i < n; i++) {
+        console.log(createLine(n, i));
+    }
 }
 
+// Takes in a line length and a number of stars in the line
+// Returns a string with length length and stars right-aligned and left-padded
+function createLine(length, stars) {
+    let line = '';
+    for (let i = 1; i <= length; i++) {
+        line += i < length - stars ? ' ' : '*';
+        
+        /*if (i < length - stars) {
+            line += ' ';
+        }
+        else {
+            line += '*';
+        }*/
+    }
+    return line;
+}
 
 module.exports = drawStairs;
