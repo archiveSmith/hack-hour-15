204dadad88e972bc00073bb1137225ba5fe64e82
ibramos <i.am.ian.ramos@gmail.com>
2017-06-10 09:35:08 -0700

    finished modemean

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -11,7 +11,14 @@
 
 
 function modemean(array) {
-
+    let cache = {}
+    let mean = (array.reduce(function(acc, val) {return acc + val;}, 0)) / array.length
+    for (let i = 0; i<array.length; i++) {
+        if(!cache[array[i]]) cache[array[i]] = 1;
+        if(cache[array[i]]) cache[array[i]] ++;
+    }
+    let mode = Object.keys(cache).reduce(function(a, b){ return cache[a] > cache[b] ? a : b });
+    return mode === mean ? true : false;
 }
 
 module.exports = modemean;
