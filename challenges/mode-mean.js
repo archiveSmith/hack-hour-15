25acd28f407eada7e3ac36fba4a66b0aa29d496d
Jeff Yamamoto <jeffmagician@gmail.com>
2017-06-10 09:44:32 -0700

    Implement mode-mean

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -9,9 +9,38 @@
  *
  */
 
-
 function modemean(array) {
+    let modes = findMaxMode(array)
+    let mean = findMean(array) 
 
+    return modes.indexOf(Math.floor(mean)) !== -1
 }
 
+function findMaxMode(array){
+    let categorized = array.reduce((acc, e) => {
+        if (!acc[e]){acc[e] = 1;}
+        else { acc[e] += 1;}
+        return acc;
+    }, {})
+    let sortedKeysByCount = Object.keys(categorized).sort((a, b) =>{ return categorized[a] < categorized[b]; })
+    let mode = sortedKeysByCount.reduce((acc, key, i, keys) => {
+        if (i === 0 || (categorized[keys[0]] === categorized[key])){
+            acc.push(parseInt(key));
+        }
+        return acc;
+    }, [])
+    return mode;
+}
+
+function findMean(array){
+    if (array.length === 0) return 0;
+    let sum = array.reduce((sum, e) =>{
+        return sum + e;
+    }, 0)
+    return sum / array.length
+}
+
+// console.log(modemean([1, 1, 2, 2, 2, 3, 3]))
+// console.log(modemean([1, 1, 1, 2, 2, 3, 3, 3]))
+// console.log(modemean([1, 1, 2, 2, 2, 3, 3]))
 module.exports = modemean;
