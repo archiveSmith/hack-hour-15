d2e4185bb6f474ec516b06810a2e12fcacae9ec5
Jeff Yamamoto <jeffmagician@gmail.com>
2017-06-13 09:44:05 -0700

    Implement kth

--- challenges/kth-to-last-node.js
+++ challenges/kth-to-last-node.js
@@ -22,7 +22,30 @@ function Node(val) {
 }
 
 function kthToLastNode(k, head) {
-
+  if (!head){ return undefined }
+  const list = []
+  let curNode = head
+  while(curNode){
+    list.push(curNode)
+    curNode = curNode.next
+  }
+  let index = list.length - k
+  let targetNode = list[index]
+  return (targetNode) ? targetNode.value : undefined
 }
+
+// const a = new Node('A');
+// const b = new Node('B');
+// const c = new Node('C');
+// const d = new Node('D');
+// const e = new Node('E');
+// a.next = b;
+// b.next = c;
+// c.next = d;
+// d.next = e;
+// const f = new Node('F')
+// console.log(kthToLastNode(2, b)) // D
+// console.log(kthToLastNode(2, f)) // undefined
+// console.log(kthToLastNode(1, f)) // F
 
 module.exports = {Node: Node, kthToLastNode: kthToLastNode};
