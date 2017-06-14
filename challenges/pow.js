98b3175afaf3690bd83a9ac143ced3fc42ba2745
Janelle Wong <janelle.wg@gmail.com>
2017-06-14 09:55:11 -0700

    Pow Challenge

--- challenges/pow.js
+++ challenges/pow.js
@@ -3,7 +3,19 @@
  */
 
 function pow(base, power) {
+	//set base case
+  if(power === 0) { 
+  	return 1;
+  } else {
+  	return base * pow(base, power - 1);
+  }
 
 }
+
+console.log(pow(3,3));
+console.log(pow(3,0));
+console.log(pow(0,4));
+
+
 
 module.exports = pow;
