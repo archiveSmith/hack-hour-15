2fe2772667d9f5c3fd8e74cd284119b2646f10fe
OnuPete <pcr137@case.edu>
2017-06-14 09:47:18 -0700

    pow wowed

--- challenges/pow.js
+++ challenges/pow.js
@@ -3,7 +3,12 @@
  */
 
 function pow(base, power) {
-
+  if (power === 0) return 1;
+  if (power === 1) return base;
+  if (power < 0) return (1 / (base * pow(base, Math.abs(power)-1)));
+  return base * pow(base, power - 1);
 }
+
+console.log(pow(3,-1))
 
 module.exports = pow;
