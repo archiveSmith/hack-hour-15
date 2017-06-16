2979d80197ff56ef6d0cf45bd1487af65fbf0afc
Brian Schmalz <bschmalz@Brians-MacBook-Air.local>
2017-06-16 09:32:17 -0700

    Finished reverse array.

--- challenges/pow.js
+++ challenges/pow.js
@@ -13,9 +13,12 @@
    // If > 1, return x * function called with x and (y - 1); 
 
 function pow(base, power) {
-	if (power < 1) return 0; 
+	if (power < 1) return 1; 
 	if (power === 1) return base; 
 	return base * pow(base, power - 1); 
 }
+
+console.log(pow(4,0)); 
+console.log(Math.pow(4,0));
 
 module.exports = pow;
