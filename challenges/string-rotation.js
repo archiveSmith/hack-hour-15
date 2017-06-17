e1eab2b75053042c568e20efed610eb5c95f691e
Garrett Levy <garrettlr@gmail.com>
2017-06-17 10:13:37 -0700

    finished substring

---
+++ challenges/string-rotation.js
@@ -0,0 +1,39 @@
+/* Given two strings, write a function to check if the second one is a rotation of the first,
+ * using only one call to isSubstring. The function isSubstring takes in two strings and returns
+ * true if the second string is found in the first, false otherwise.
+ *
+ * h->e->l->l->o   ===   l->l-o->h->e
+ * h->e->l->l->o   !==   o->l-l->h->e
+ *
+ * For example: stringRotation("hello", "hello") -> true
+ *              stringRotation("hello", "llohe") -> true
+ hellohello
+lohel
+lohle
+ohlle
+ *              stringRotation("hello", "he") -> false
+ *              stringRotation("hello", "ollhe") -> false (not a rotation, just an anagram)
+ */
+
+function isSubstring(s1, s2) {
+  return s1.indexOf(s2) >= 0;
+}
+
+function stringRotation(s1, s2) {
+  if (s1.length !== s2.length) return false;
+  s1 = s1 + s1;
+  return isSubstring(s1, s2);
+}
+
+let test = stringRotation("hello", "hello") //-> true
+console.log(test);
+test = stringRotation("hello", "llohe")// -> true
+console.log(test);
+test = stringRotation("hello", "he") //-> false
+console.log(test);
+test = stringRotation("hello", "ollhe")// -> false
+console.log(test);
+
+
+//console.log(stringRotation('hello', 'olleh'));
+module.exports = {isSubstring: isSubstring, stringRotation: stringRotation};
