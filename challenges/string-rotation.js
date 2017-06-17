bf68bd97e5b5cb0b364cc3a9fd7d7323c598d696
Jeremy S. Lee <jeremysanglee@gmail.com>
2017-06-17 09:53:17 -0700

    Complete string-rotation.js

--- challenges/string-rotation.js
+++ challenges/string-rotation.js
@@ -4,7 +4,18 @@
  *
  * h->e->l->l->o   ===   l->l-o->h->e
  * h->e->l->l->o   !==   o->l-l->h->e
- *
+ 
+ hello
+ elloh
+ llohe
+ lohel
+ ohell
+ 
+ bye
+ yeb
+ eby
+
+
  * For example: stringRotation("hello", "hello") -> true
  *              stringRotation("hello", "llohe") -> true
  *              stringRotation("hello", "he") -> false
@@ -16,7 +27,19 @@ function isSubstring(s1, s2) {
 }
 
 function stringRotation(s1, s2) {
+  if (s1.length !== s2.length || typeof s1 !== typeof s2) {
+    return false;
+  }
+  let temp1 = '';
+  let temp2 = '';
+  let len = s1.length;
+  let rotationStateAt0 = s2.indexOf(s1[0]);
+  let rotationStateAt1 = s2.indexOf(s1[1]);
+  
+  temp1 = s1.slice(2);
+  temp2 = s2.slice(rotationStateAt1 + 1) + s2.slice(0, rotationStateAt0);
 
+  return isSubstring(temp1, temp2);
 }
 
 module.exports = {isSubstring: isSubstring, stringRotation: stringRotation};
