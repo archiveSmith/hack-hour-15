b449ca0e74f9969a3677af936cb7e28c46ed6b71
mm0nr0e <monroe.mirand@gmail.com>
2017-06-17 10:08:31 -0700

    hackhour 6-17

--- challenges/string-rotation.js
+++ challenges/string-rotation.js
@@ -14,9 +14,16 @@
 function isSubstring(s1, s2) {
   return s1.indexOf(s2) >= 0;
 }
-
 function stringRotation(s1, s2) {
+  //for each index of s2[0] in s1, split s1 at index
+  if (s1.length !== s2.length) return false;
 
+  for (let i = 0; i < s1.length; i += 1) {
+    if (s1[i] === s2[0]) {
+      if (s1.slice(i) === s2.slice(0, s1.length-i) && s2.slice(s1.length - i) === s1.slice(0, i)) return true;
+    }
+  }
+
+  return false;
 }
-
 module.exports = {isSubstring: isSubstring, stringRotation: stringRotation};
