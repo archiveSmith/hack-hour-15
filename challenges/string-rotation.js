1e84ec5f77cba5d6d20f19b767a40c82d7c9c259
Henry (Hai) Zhu <henryhaizhu@gmail.com>
2017-06-16 21:59:00 -0700

    challenges

--- challenges/string-rotation.js
+++ challenges/string-rotation.js
@@ -11,12 +11,40 @@
  *              stringRotation("hello", "ollhe") -> false (not a rotation, just an anagram)
  */
 
+
+
 function isSubstring(s1, s2) {
   return s1.indexOf(s2) >= 0;
 }
 
-function stringRotation(s1, s2) {
 
+function stringRotation(s1, s2) {
+  if (s2.length !== s1.length || arguments.length<2){
+    return false;
+  }
+  if (s1 === s2){
+    return true;
+  }
+  let matchFound = false,
+      check;
+  s2 = s2.split("");
+  for (let i=0; i<s2.length; i++){
+    let firstChar = s2[0];
+    s2.forEach((ele, index, arr)=>{
+      if(index === s2.length-1){
+        arr[index] = firstChar;
+      }else{
+        arr[index] = arr[index+1]
+      }
+    })
+    check = s2.join("");
+    if(check === s1){
+      matchFound = true;
+    }
+  }
+  return matchFound;
 }
+
+
 
 module.exports = {isSubstring: isSubstring, stringRotation: stringRotation};
