ccb5af0835c2a0e3b9077aa37eb5947e427ee65f
Melissa Schwartz <melissjs@gmail.com>
2017-06-17 10:08:16 -0700

    not working, but I think I know why

--- challenges/string-rotation.js
+++ challenges/string-rotation.js
@@ -12,11 +12,37 @@
  */
 
 function isSubstring(s1, s2) {
-  return s1.indexOf(s2) >= 0;
+  if (s1.indexOf(s2) >= 0){
+    let ind = s1.indexOf(s2);
+    for (let ii=ind; ii<sr.length; ii++){
+      let s2Ind =0;
+      if (s2[s2Ind] === s1[ii]){
+        s2Ind++;
+      } else {
+        let endSubs1 = s1[ii-1]
+        let endSubs2 = s2[s2Ind];
+        for (let iii=0; iii<s2Ind; iii++){
+        if (s2[endSubs2+1+iii]=== s1[iii]){
+          return true
+        } else return false;
+        }
+      }
+    }
+  }
+
 }
 
 function stringRotation(s1, s2) {
-
+  if (s1[0] !== s2[0]){
+return isSubstring(s1, s2)
+  } else if (s1=s2){
+    return true;
+  } else {
+    return false;
+  }
 }
+
+let str = "melissa";
+console.log(stringRotation("hello", "lohel"));
 
 module.exports = {isSubstring: isSubstring, stringRotation: stringRotation};
