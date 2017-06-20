56e93413e6ef09d554eafa7ea7585d4cced59b20
pennwu1 <phw.dental@gmail.com>
2017-06-20 10:11:05 -0700

    complete balanced-parens

--- challenges/balanced-parens.js
+++ challenges/balanced-parens.js
@@ -24,8 +24,45 @@
  *
  */
 
-function balancedParens(input){
 
+// Given a string of text, return true if the parentheses are balanced
+// and false otherwise.
+
+// Find the position of parentheses in the string
+// If l + r = string length, return true
+// Do this for [ and {
+// Ignore non-bracket items
+
+
+function balancedParens(input) {
+    let boolean = true;
+    let copiedInput = input.slice();
+    copiedInput = copiedInput.replace(/[^()\[\]\{\}]/gi, '');
+    console.log(copiedInput);
+    let length = copiedInput.length;
+    // If l + r = string length, return true
+    for (let i = 0; i < length; i++) {
+        if (copiedInput[i] === '(') {
+            boolean = copiedInput[length - i - 1] === ')' && i < length - i - 1;
+        }
+        if (copiedInput[i] === '[') {
+            boolean = copiedInput[length - i - 1] === ']' && i < length - i - 1;
+        }
+        if (copiedInput[i] === '{') {
+            boolean = copiedInput[length - i - 1] === '}' && i < length - i - 1;
+        }
+    }
+    return boolean;
 }
+
+ console.log(balancedParens('(')); // false
+ console.log(balancedParens('()')); // true
+ console.log(balancedParens(')(')); // false
+ console.log(balancedParens('(())')); // true
+ console.log(balancedParens('[](){}')); // true
+ console.log(balancedParens('[({})]'));   // true
+ console.log(balancedParens('[(]{)}')); // false
+ console.log(balancedParens(' var wow  = { yo: thisIsAwesome() }')); // true
+ console.log(balancedParens(' var hubble = function() { telescopes.awesome();')); // false
 
 module.exports = balancedParens;
