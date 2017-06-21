575e708cf616be7ad624378e07b1616305e4da24
KseniaAK <ksenia.kozh@gmail.com>
2017-06-21 11:12:17 -0700

    roman numerals

--- challenges/roman-numeral.js
+++ challenges/roman-numeral.js
@@ -14,11 +14,33 @@
  *      500   ->    D
  *      900   ->    CM
  *      1000  ->    M
- * 
+ *
  */
 
-function romanNumeral(n) {
+ function romanNumeral(n) {
+   let romanN = [];
+   const conversions = {
+     1: 'I',
+     5: 'V',
+     10: 'X',
+     50: 'L',
+     100: 'C',
+     500: 'D',
+     1000: 'M'
+   };
+   const tenQuotient = Math.floor(n / 10);
+   if (tenQuotient > 0 && tenQuotient < 4) {
+     for (let i = 0; i < tenQuotient; i++) romanN.push(conversions[10]);
+   }
+   const tenRemainder = n % 10;
+   if (tenRemainder > 0 && tenRemainder < 4) {
+     for (let i = 0; i < tenRemainder; i++) romanN.push(conversions[1]);
+   } else if (tenRemainder === 4 || tenRemainder === 9) {
+     romanN.push(conversions[1] + conversions[tenRemainder + 1]);
+   }
+   const fiveQuotient = Math.floor(n / 5);
 
-}
+   return romanN.join('');
+ }
 
 module.exports = romanNumeral;
