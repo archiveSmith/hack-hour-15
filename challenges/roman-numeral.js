566dded380baa6f2517a87e026e3cf39251ea9b5
Brett Beekley <brettbeekley@gmail.com>
2017-06-21 09:40:38 -0700

    Working except for things like 4 => IV

--- challenges/roman-numeral.js
+++ challenges/roman-numeral.js
@@ -17,8 +17,45 @@
  * 
  */
 
-function romanNumeral(n) {
+// Input
+  // Positive integer
 
+// Ouput
+  // String containing the integer in Roman numeral form
+
+function romanNumeral(n) {
+  const converstions = [
+    {letter: 'M', val: 1000},
+    {letter: 'D', val: 500},
+    {letter: 'C', val: 100},
+    {letter: 'L', val: 50},
+    {letter: 'X', val: 10},
+    {letter: 'V', val: 5},
+    {letter: 'I', val: 1},
+  ];
+  
+  // Set the current number to the input value
+  let cur = n;
+  // Create an empty output string
+  let output = '';
+  // Iterate through the conversions array
+  converstions.forEach((numeral) => {
+    //console.log(cur, numeral);
+    // Store how many times the value goes into the current number
+    const factor = Math.floor(cur / numeral.val);
+    // Append the letter that many times to the output string
+    for (let i = 0; i < factor; i += 1) {output += numeral.letter}
+    // Set the current number to remainder (current % value)
+    cur = cur % numeral.val;
+  });
+  // Return the output string
+  return output;
 }
+
+// Test cases
+console.log(romanNumeral(1)); // I
+console.log(romanNumeral(5)); // V
+console.log(romanNumeral(115)); // CXV
+
 
 module.exports = romanNumeral;
