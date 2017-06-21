ee3df853ca05483b68fbb5f692a13f1bff7daa14
Henry (Hai) Zhu <henryhaizhu@gmail.com>
2017-06-21 09:50:15 -0700

    romanNum

--- challenges/roman-numeral.js
+++ challenges/roman-numeral.js
@@ -17,8 +17,45 @@
  * 
  */
 
-function romanNumeral(n) {
 
+
+function romanNumeral(n) {
+    let result = [];
+    let rObj = {
+        I: 1,
+        IV: 4,
+        V: 5,
+        IX: 9,
+        X: 10,
+        XL: 40,
+        L: 50,
+        XC: 90,
+        C: 100,
+        CD: 400,
+        D: 500,
+        CM: 900,
+        M: 1000
+    }
+    let keys = Object.keys(rObj);
+    for (let i = keys.length-1; i>=0; i--){
+        console.log(rObj[keys[i]])
+        if(n >= rObj[keys[i]]){
+          if(n> 1000){
+              while(n>=1000){
+              result.push([keys[i]])
+              n -= 1000;
+              }
+              if(n === 0) return result.join("");
+          }
+          else{
+            result.push([keys[i]])
+            n -= rObj[keys[i]];
+            if(n === 0) return result.join("");
+          }
+            
+        }
+    }
+    return result.join("");
 }
 
 module.exports = romanNumeral;
