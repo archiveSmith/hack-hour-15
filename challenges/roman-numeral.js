2f3f00a26104f8d803db72a59e573e8d64e51ffa
Christopher Dobler <chrisdobler@gmail.com>
2017-06-21 09:39:10 -0700

    Adds Roman Numerals

--- challenges/roman-numeral.js
+++ challenges/roman-numeral.js
@@ -18,7 +18,73 @@
  */
 
 function romanNumeral(n) {
+  let l = '';
+  let r = n;
+  do {
+    if(r >= 1000) {
+      r -= 1000
+      l += 'M';
+      continue;
+    }
+    if(r >= 900) {
+      r -= 900
+      l += 'CM';
+      continue;
+    }
+    if(r >= 500) {
+      r -= 500
+      l += 'D';
+      continue;
+    }
+    if(r >= 400) {
+      r -= 400
+      l += 'CD';
+      continue;
+    }
+    if(r >= 100) {
+      r -= 100
+      l += 'C';
+      continue;
+    }
+    if(r >= 90) {
+      r -= 90
+      l += 'XC';
+      continue;
+    }
+    if(r >= 40) {
+      r -= 40
+      l += 'XL';
+      continue;
+    }
+    if(r >= 10) {
+      r -= 10
+      l += 'X';
+      continue;
+    }
+    if(r >= 9) {
+      r -= 9
+      l = 'IX';
+      continue;
+    }
+    if(r >= 5) {
+      r -= 5
+      l += 'V';
+      continue;
+    }
+    if(r >= 4) {
+      r -= 4
+      l += 'IV';
+      continue;
+    }
+    if(r >= 1) {
+      r -= 1
+      l += 'I';
+      continue;
+    }
+    break;
+  } while (true);
 
+  return l;
 }
 
 module.exports = romanNumeral;
