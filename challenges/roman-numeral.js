792e170168912a28f9a7c0db4aba6656d086dcde
Louis Rouaze <louisrouaze@Macintosh-3.local>
2017-06-21 10:55:23 -0700

    hackhour

--- challenges/roman-numeral.js
+++ challenges/roman-numeral.js
@@ -18,8 +18,6 @@
  */
 
 function romanNumeral(n) {
-if (isNaN(n) === true || n === null) return undefined;
-
 	const roman =  {"M" :1000, "CM":900, "D":500, "CD":400, "C":100, "XC":90, "L":50, "XL":40, "X":10, "IX":9, "V":5, "IV":4, "I":1};
     const str = "";
 
