fbb75ff47e4168996a10b84a843cb69bbbc62946
icrustandi <icrustandi@gmail.com>
2017-06-21 09:53:46 -0700

    Finished roman-numeral

--- challenges/roman-numeral.js
+++ challenges/roman-numeral.js
@@ -17,8 +17,64 @@
  * 
  */
 
-function romanNumeral(n) {
+function romanTransform(n) {
+  switch (n) {
+    case 1000:
+      return 'M';
+      break;
+    case 900:
+      return 'CM';
+      break;
+    case 500:
+      return 'D';
+      break;
+    case 400:
+      return 'CD';
+      break;
+    case 100:
+      return 'C';
+      break;
+    case 90:
+      return 'XC';
+      break;
+    case 50:
+      return 'L';
+      break;
+    case 40:
+      return 'XL';
+      break;
+    case 10:
+      return 'X';
+      break;
+    case 9:
+      return 'IX';
+      break;
+    case 5:
+      return 'V';
+      break;
+    case 4:
+      return 'IV';
+      break;
+    case 1:
+      return 'I';
+      break;
+  }
+}
 
+function romanNumeral(n) {
+  const denominations = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+  let numToRomanize = n;
+  let romanNumeral = '';
+  let biggestDenomIndex = 0;
+  while (numToRomanize !== 0) {
+    if (numToRomanize >= denominations[biggestDenomIndex]) {
+      romanNumeral += romanTransform(denominations[biggestDenomIndex]);
+      numToRomanize -= denominations[biggestDenomIndex];
+    } else {
+      biggestDenomIndex += 1;
+    }
+  }
+  return romanNumeral;
 }
 
 module.exports = romanNumeral;
