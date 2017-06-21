29270bae0cbf367f9580e72b43420153503d109d
pennwu1 <phw.dental@gmail.com>
2017-06-21 10:01:27 -0700

    complete roman-numeral challenge

--- challenges/roman-numeral.js
+++ challenges/roman-numeral.js
@@ -17,8 +17,37 @@
  * 
  */
 
-function romanNumeral(n) {
 
+// Input: Number
+// Output: Roman Numeral
+
+// Given a number, return the roman numeral equivalent
+// Start at single digits
+    // Store our single digits in an array (0 is an empty string)
+    // Store any denominations of 5's and 10's
+// Use recursion to go from thousands => single digits
+
+function romanNumeral(n) {
+    let output = '';
+    let singles = ['', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX'];
+    let tens = ['X', 'XX', 'XXX', 'XL', 'L', 'LX', 'LXX', 'LXXX', 'XC'];
+    let hundreds = ['C', 'CC', 'CCC', 'CD', 'D', 'DC', 'DCC', 'DCCC', 'CM'];
+    let thousands = ['M', 'MM', 'MMM'];
+    if (n < 10) {
+        return singles[n];
+    }
+    if (n >= 10 && n < 100) {
+        // how to get the single digits?
+        return tens[Math.floor(n/10) - 1]  + romanNumeral(n - Math.floor(n/10) * 10);
+    }
+    if (n < 1000) {
+        return hundreds[Math.floor(n/100) - 1] + romanNumeral(n - Math.floor(n/100) * 100);
+        // get the hundred position
+    }
+    if (n > 1000 && n < 4000) {
+        return thousands[Math.floor(n/1000) - 1] + romanNumeral(n- Math.floor(n/1000) * 1000);
+    }
 }
+
 
 module.exports = romanNumeral;
