625934cc69257b5f061ace4b2fafe3c5b0553ce5
Brett Beekley <brettbeekley@gmail.com>
2017-06-24 09:38:51 -0700

    Progress on extension

--- challenges/bin-to-dec.js
+++ challenges/bin-to-dec.js
@@ -36,10 +36,49 @@ function binToDec(binary) {
 }
 
 // Test cases
-console.log(binToDec('0'))    // -> 0
-console.log(binToDec('11'))   // -> 3
-console.log(binToDec('100'))  // -> 4
-console.log(binToDec('101'))  // -> 5
-console.log(binToDec('0101')) // -> 5
+//console.log(binToDec('0'))    // -> 0
+//console.log(binToDec('11'))   // -> 3
+//console.log(binToDec('100'))  // -> 4
+//console.log(binToDec('101'))  // -> 5
+//console.log(binToDec('0101')) // -> 5
+//console.log(binToDec('1010101110101')) // -> 5493
+
+// Input
+  // Integer
+// Output
+  // String that is the input in binary
+
+function decToBin(num) {
+  
+  // Declare an output string
+  let output = '';
+  
+  // Find the largest power of two that goes into num
+    let largestPower; // not sure how to do this
+  
+  // Add a 1 to the output string
+  output += '1';
+  
+  // Subtract the power from the numer
+  num -= largestPower;
+  
+  function powerOfTwo(num, power) {
+    return num >= Math.pow(2, power);
+  }
+  
+  // Iterate from the largest power -1 to zero
+  
+    // If the 2^i is less than the number
+      
+      // Add a 1 to the output string
+  
+      // Subtract the power from the numer
+  
+    // Else 
+      
+      // Add a 0 to the output string
+  
+  // Return the output string
+}
 
 module.exports = binToDec;
