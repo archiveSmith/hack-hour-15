0717817cd7bbb0f64415e92fc26be943b7852f01
Jeff Yamamoto <jeffmagician@gmail.com>
2017-06-27 10:01:04 -0700

    Implement match word

--- challenges/match-word.js
+++ challenges/match-word.js
@@ -8,7 +8,29 @@
 // matchWord('');  -> true
 
 function matchWord(str) {
+  if (str.split('').length === 0) return true
+  const myRe = /[A-Za-z0-9]+/g;
+  let myArray
+  const result = []
+  while(myArray = myRe.exec(str)){
+    result.push(myArray[0])
+  }
+  if (result.length % 2 === 1) return false
+  const stack = [];
+  // for(let i = 0; i < result.length; i++){
+    // if (i === 0) { stack.push(result[0]); next}
+    // if (stack[stack.length - 1]){}
+  // }
+  let combined = result.join('').toLowerCase()
+  console.log(combined)
+  return combined === combined.split('').reverse().join('')
 
 }
+// console.log(matchWord('__END_DNE-----'))//  -> true
+// console.log(matchWord('__ENDDNE__'))//  -> false       (not separated by a space)
+// console.log(matchWord('IF()()fi[]'))//  -> true        (should be case-insensitive)
+// console.log(matchWord('for__if__rof__fi'))//  -> false     not properly closed. like ( [) ] 
+// console.log(matchWord('%%$@$while  try ! yrt  for if_fi rof #*#  elihw'))//  -> true
+// console.log(matchWord(''))//  -> true
 
 module.exports = matchWord;
