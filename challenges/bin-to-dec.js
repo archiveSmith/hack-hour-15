343db038e62d7dd47cc4b52d3a39be6123a6391a
icrustandi <icrustandi@gmail.com>
2017-06-27 09:23:43 -0700

    Store hex attempt

--- challenges/bin-to-dec.js
+++ challenges/bin-to-dec.js
@@ -25,49 +25,62 @@ function binToDec(binary) {
   return decSum;
 }
 
-function hexadecimal(binary) {
-  let power = binary.length - 1
-  let hexDigit = 0;
-  let hexadecimal = '';
-  // iterate thru string from the end
-  for (let i = binary.length - 1; i >= 0; i -= 1) {
-    if (binary[i] === '1') {
-      hexDigit += Math.pow(2, power - i);
-      if (hexDigit > 15) {
-        hexDigit -= Math.pow(2, power - i);
-        if (hexDigit > 9) hexadecimal = letters(hexDigit) + hexadecimal;
-        else hexDigit = hexDigit.toString() + hexadecimal;
-      }
-    }
-  }
-  return hexadecimal;
-}
+// function hexadecimal(binary) {
+//   let power = binary.length - 1
+//   let hexDigit = 0;
+//   let hexadecimal = '';
+//   // iterate thru string from the end
+//   for (let i = binary.length - 1; i >= 0; i -= 1) {
+//     if (binary[i] === '1') {
+//       hexDigit += Math.pow(16, power - i);
+//       //fix this
+//       if (hexDigit > 15) {
+//         hexDigit -= Math.pow(2, power - i);
+//         if (hexDigit > 9) hexadecimal = letters(hexDigit) + hexadecimal;
+//         else hexadecimal = hexDigit.toString() + hexadecimal;
+        
+//         // console.log(hexadecimal)
+//       }
+//     }
+//   }
 
-function letters (hexdigit) {
-  switch (hexDigit) {
-  case 10:
-    hexDigit = 'A';
-    break;
-  case 11:
-    hexDigit = 'B';
-    break;
-  case 12:
-    hexDigit = 'C';
-    break;
-  case 13:
-    hexDigit = 'D';
-    break;
-  case 14:
-    hexDigit = 'E';
-    break;
-  case 15:
-    hexDigit = 'F';
-    break;
-  }
+//   if (hexDigit > 9) hexadecimal = letters(hexDigit) + hexadecimal;
+//   else hexadecimal = hexDigit.toString() + hexadecimal;
+//   return hexadecimal;
+// }
+
+// function letters (hexdigit) {
+//   switch (hexdigit) {
+//     case 10:
+//       hexdigit = 'A';
+//       break;
+//     case 11:
+//       hexdigit = 'B';
+//       break;
+//     case 12:
+//       hexdigit = 'C';
+//       break;
+//     case 13:
+//       hexdigit = 'D';
+//       break;
+//     case 14:
+//       hexdigit = 'E';
+//       break;
+//     case 15:
+//       hexdigit = 'F';
+//       break;
+//   }
+//   return hexdigit
+// }
+
+function hexadecimal(binary) {
+  // slice by 4 from the end
+  // map 0 to F
+  switch (binary)
 }
 module.exports = binToDec;
 console.log(hexadecimal('0'))   //-> 0
  console.log(hexadecimal('11'))  //-> 3
  console.log(hexadecimal('100')) //-> 4
  console.log(hexadecimal('101')) //-> 5
- console.log(hexadecimal('0101')) //-> 5
+ console.log(hexadecimal('1101111')) //-> D
