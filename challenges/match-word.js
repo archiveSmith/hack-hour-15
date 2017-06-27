c3230162c51cce7d71ec0891168d847ecbb6c024
OnuPete <pcr137@case.edu>
2017-06-27 09:54:08 -0700

    match those closing words

--- challenges/match-word.js
+++ challenges/match-word.js
@@ -3,12 +3,24 @@
 // matchWord('__END_DNE-----');  -> true
 // matchWord('__ENDDNE__');  -> false       (not separated by a space)
 // matchWord('IF()()fi[]');  -> true        (should be case-insensitive)
-// matchWord('for__if__rof__fi');  -> false     not properly closed. like ( [) ] 
+// matchWord('for__if__rof__fi');  -> false     not properly closed. like ( [) ]
 // matchWord('%%$@$while  try ! yrt  for if_fi rof #*#  elihw');  -> true
 // matchWord('');  -> true
 
 function matchWord(str) {
+  if (typeof str !== 'string') return 'enter valid string';
+  if (str.length === 0) return true;
+  const strarr = str.split(/[\W\s]|_/g);
+  const words = [];
 
+  for (let i = 0; i < strarr.length; i++) {
+    if (strarr[i] === '') continue;;
+    if (words.length !== 0 && strarr[i].toLowerCase().split('').reverse().join('') === words[words.length - 1]) {
+      words.pop();
+    } else words.push(strarr[i].toLowerCase());
+  }
+  return words.length === 0;
 }
+
 
 module.exports = matchWord;
