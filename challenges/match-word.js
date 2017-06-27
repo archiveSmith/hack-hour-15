084853edf20c3c77014473a9e2b12874423a42c2
pennwu1 <phw.dental@gmail.com>
2017-06-27 10:02:06 -0700

    match-word attempt 1

--- challenges/match-word.js
+++ challenges/match-word.js
@@ -7,8 +7,56 @@
 // matchWord('%%$@$while  try ! yrt  for if_fi rof #*#  elihw');  -> true
 // matchWord('');  -> true
 
-function matchWord(str) {
 
+// Given a string, check if the words inside the string are closed by their backwards counterparts
+    // Case Insensitive
+    // Separated by special character or space
+    // Order matters
+
+// Approach 1:
+    // Iterate through the string
+        // Add strings to a data structure
+        // The data structure should have an even length
+        // Numbers at opposite positions should equal their reversed counterparts
+
+// Approach 2:
+function matchWord(str) {
+    // Iterate through the string with a for loop
+    let arr = [];
+    let string = '';
+    for (let i = 0; i < str.length; i++) {
+        // Regex Method, add string if 
+        if (/[A-Za-z]/.test(str[i])) string += str[i];
+        else if (string.length) {
+            arr.push(string);
+            string = '';
+        }
+    }
+    if (string.length) arr.push(string);
+    checkReverseWords(arr);
+    // Add letters to a string
+    // Reset the string when the next character is not a letter
+    // Check if words at opposite positions of the array equal their reversed counterparts
+    
 }
+
+function checkReverseWords(arr) {
+    let boolean = true;
+    for (let i = 0; i < arr.length; i++) {
+        if (!arr[i] === arr[arr.length - i - 1].split().reverse().join()) {
+            boolean = false;
+        }
+    }
+    return boolean;
+}
+
+
+
+console.log(matchWord('__END_DNE-----')); //  -> true
+console.log(matchWord('__ENDDNE__')); // -> false       (not separated by a space)
+console.log(matchWord('IF()()fi[]')); // -> true        (should be case-insensitive)
+console.log(matchWord('for__if__rof__fi')); //  -> false     not properly closed. like ( [) ] 
+console.log(matchWord('%%$@$while  try ! yrt  for if_fi rof #*#  elihw')); //  -> true
+console.log(matchWord('')); //  -> true
 
 module.exports = matchWord;
