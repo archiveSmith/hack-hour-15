06fed58f48ce564a20c3d751ad2faf9dbcca66b1
ibramos <i.am.ian.ramos@gmail.com>
2017-06-27 10:58:10 -0700

    need to rework logic to properly implement regex

--- challenges/match-word.js
+++ challenges/match-word.js
@@ -8,7 +8,29 @@
 // matchWord('');  -> true
 
 function matchWord(str) {
-
+    if (str === '') return true;
+    const lowerCase = str.toLowerCase().split('');
+    const reverse = str.toLowerCase().split('').reverse();
+    const lowerWord = [];
+    const reverseWord = [];
+    for (let i = 0; i< lowerCase-1; i++) {
+        if(lowerCase[i].includes(/[a-z]/)) {
+            lowerWord.push(lowerCase[i]);
+            if(!lowerCase[i+1].includes(/[a-z]/)) {
+                break;
+            }
+        }
+    }
+    for (let i = 0; i< reverse -1; i++) {
+        if(reverse[i].includes(/[a-z]/)) {
+            reverseWord.push(reverse[i]);
+            if(!reverse[i+1].includes(/[a-z]/)) {
+                break;
+            }
+        }
+    }
+    return lowerWord.join('') === reverseWord.join('')
 }
+
 
 module.exports = matchWord;
