18d5c188b8ab2cf14676d81073e9b0ccbc5d323c
icrustandi <icrustandi@gmail.com>
2017-06-28 09:21:17 -0700

    almost finished match-word

--- challenges/match-word.js
+++ challenges/match-word.js
@@ -8,7 +8,33 @@
 // matchWord('');  -> true
 
 function matchWord(str) {
+  // separated by space/punctuation
+  const matchWords = [];
+  const strArr = str.split(/([_\W])/);
+  // console.log(strArr);
+  if (str === '') return true;
+  for (let i = 0; i < strArr.length; i += 1) {
+    if (matchWords.length > 0 && strArr[i].length > 1 && matchWords[matchWords.length -1] === reverse(strArr[i])) {    matchWords.pop();
+    } else if (strArr[i].length > 1) {
+      matchWords.push(strArr[i].toLowerCase());
+    }
+  }
+  return matchWords.length === 0;
+}
 
+function reverse(string) {
+  let reverseStr = '';  
+  for (let i = string.length -1 ; i >= 0; i -= 1) {
+    reverseStr += string[i];
+  }
+  return reverseStr;
 }
 
 module.exports = matchWord;
+console.log(matchWord('__END_DNE-----'));  //-> true
+console.log(matchWord('__ENDDNE__'));  //-> false       (not separated by a space)
+console.log(matchWord('IF()()fi[]'));  ///-> true        (should be case-insensitive)
+console.log(matchWord('for__if__rof__fi'));  //-> false     not properly closed. like ( [) ] 
+console.log(matchWord('%%$@$while  try ! yrt  for if_fi rof #*#  elihw'));  //-> true
+console.log(matchWord(''));  //-> true
+
