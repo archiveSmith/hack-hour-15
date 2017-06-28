9ba9c65f51b3a5e66d04085379567e59159b414c
Christopher Dobler <chrisdobler@gmail.com>
2017-06-28 09:47:41 -0700

    Adds Highest Product

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,7 +3,20 @@
  */
 
 function highestProduct(array) {
-
+  if(!Array.isArray(array)) return undefined;
+  let max = 0;
+  for(let i = 0; i < array.length; i += 1) {
+    for(let j = 0; j < array.length; j += 1) {
+      for(let k = 0; k < array.length; k += 1) {
+        if(i !== j && j !== k && k !== i) {
+          let product = array[i] * array[j] * array[k];
+          console.log(array[i]+' * '+array[j]+' * '+array[k]+' = '+product);
+          if (product > max) max = product;
+        }
+      }
+    }
+  }
+  return max;
 }
 
 
