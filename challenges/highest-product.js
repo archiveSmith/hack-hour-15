869af64ebadc195d0dc80cf896cd0e1c978518c6
Brett Beekley <brettbeekley@gmail.com>
2017-06-28 09:45:41 -0700

    Commented out test cases

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -14,14 +14,6 @@ function highestProduct(array) {
     return a - b;
   }).reverse();
   
-//  // if there are no positive numbers
-//  if (array[0] <= 0) {
-//    // return the three most negative numbers
-//    return array[array.length - 1] * array[array.length - 2] * array[array.length - 3]
-//  }
-  
-  console.log(sorted);
-  
   // If the product of the second and third highest positive numbers is greater than the product of the first and second highest negative numbers
   if (sorted[1] * sorted[2] > sorted[sorted.length - 1] * sorted[sorted.length - 2]) {
   
@@ -38,10 +30,10 @@ function highestProduct(array) {
 
 // test cases
 
-console.log(highestProduct([1,2,3,4,5])); // 60
-console.log(highestProduct([-1,-2,-3,-4,-5])); // -6
-console.log(highestProduct([1,2,3,-4,-5])); // 60
-console.log(highestProduct([-1,-2,3,4,5])); // 60
-console.log(highestProduct([-1,-2,3,4,-5])); // 40
+//console.log(highestProduct([1,2,3,4,5])); // 60
+//console.log(highestProduct([-1,-2,-3,-4,-5])); // -6
+//console.log(highestProduct([1,2,3,-4,-5])); // 60
+//console.log(highestProduct([-1,-2,3,4,5])); // 60
+//console.log(highestProduct([-1,-2,3,4,-5])); // 40
 
 module.exports = highestProduct;
