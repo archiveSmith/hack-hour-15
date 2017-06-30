646f12fd977fe2c5e50457b8c1f37e9ef5d38f18
Jerry Jong <jerryjong@Jerrys-MacBook-Pro-2.local>
2017-06-30 09:48:31 -0700

    zip-linked-lists and highest-product edge case fixed

--- challenges/zip-linked-lists.js
+++ challenges/zip-linked-lists.js
@@ -10,7 +10,32 @@ function Node(val) {
   this.next = null;
 }
 
+// edge cases
+  // if l1 or l2 is null, return the other linked list since we cannot merge
+  // if both are null, game over
+
 function zip(l1, l2) {
+  if (l1 = null) return l2;
+  if (l2 = null) return l1;
+
+  let l3 = new Node(l1);
+  let flag = false;
+
+  while(l1 && l2) {
+    if(!flag) {
+      l3.next = l2;
+      l3 = l3.next;
+      flag = true;
+    } else {
+      l3.next = l1;
+      l3 = l3.next;
+      flag = false;
+    }
+  }
+  if(!l1) l3.next = l2;
+  if(!l2) l3.next = l1;
+
+  return l3;
 };
 
 module.exports = {Node: Node, zip: zip};
