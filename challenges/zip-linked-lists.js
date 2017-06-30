774bd93f951b30bad5e906bf1c360d7b31e1ff58
OnuPete <pcr137@case.edu>
2017-06-30 11:08:42 -0700

    zip it up

--- challenges/zip-linked-lists.js
+++ challenges/zip-linked-lists.js
@@ -11,6 +11,50 @@ function Node(val) {
 }
 
 function zip(l1, l2) {
+  if (!(l1 instanceof Node) || !(l2 instanceof Node)) {
+    if (l1 instanceof Node) return l1;
+    if (l2 instanceof Node) return l2;
+    return 'Enter valid inputs';
+  }
+  let head1 = l1;
+  let head2 = l2;
+  while (head1 || head2) {
+    let tmp;
+
+    if (head1 && head1.next) tmp = head1.next;
+    if (head2) {
+      head1.next = new Node(head2.value);
+      if (tmp) {
+        head1.next.next = tmp;
+        head1 = head1.next;
+      }
+    }
+    if (head1) head1 = head1.next;
+    if (head2) head2 = head2.next;
+  }
+  return l1;
 };
+
+// const a = new Node('a');
+// const b = new Node('b');
+// const c = new Node('c');
+// const d = new Node('d');
+//
+// c.next = d;
+// b.next = c;
+// a.next = b;
+//
+// const pete = new Node('pete');
+// const chris = new Node('chris');
+// const garr = new Node('garr');
+// const penn = new Node('penn');
+//
+// garr.next = penn;
+// chris.next = garr;
+// pete.next = chris;
+//
+// console.log(pete);
+// console.log(a);
+// console.log(zip(pete, a));
 
 module.exports = {Node: Node, zip: zip};
