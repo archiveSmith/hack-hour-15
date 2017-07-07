d18360919ae8577bf2d3e3fbb82259100cb9077b
Melissa Schwartz <melissjs@gmail.com>
2017-07-07 10:14:03 -0700

    not working

--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -1,3 +1,4 @@
+'use strict'
 /**
  * Given a string, determine if any of the permutations of that string is a palindrome
  *
@@ -10,7 +11,27 @@
  */
 
 function permPalin(str) {
-	
+    let revStr = str.split('').reverse().join('');
+    let counterToThree = 0;
+    // console.log(revStr);
+    // return str.includes(revStr)? true: false;
+    // return str.indexOf(revStr) !== -1;
+    for (let i=0; i<str.length; i++){
+        for (let ii=0; ii<revStr.length; ii++){
+            let k = i;
+            if (counterToThree === 3) return true;
+            if (str[i] === revStr[ii]) {
+                counterToThree++
+                k=k+1;
+            }
+            else counterToThree = 0;
+        }
+    }
+return false;	
 }
-
+console.log(permPalin('bb')) 
 module.exports = permPalin;
+
+// var string = "foo",
+// substring = "oo";
+// string.includes(substring);
