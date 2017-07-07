b1dbcf75022694abad6af7b85f0138238cdd2ee7
OnuPete <pcr137@case.edu>
2017-07-07 10:24:52 -0700

    num to words to thousands

--- challenges/num-to-words.js
+++ challenges/num-to-words.js
@@ -13,7 +13,72 @@
  */
 
 function numToWords(num) {
+  if (typeof num !== 'number') return 'Invalid input';
+  const teens = {
+    0: 'Zero',
+    1: 'One',
+    2: 'Two',
+    3: 'Three',
+    4: 'Four',
+    5: 'Five',
+    6: 'Six',
+    7: 'Seven',
+    8: 'Eight',
+    9: 'Nine',
+    10: 'Ten',
+    11: 'Eleven',
+    12: 'Twelve',
+    13: 'Thirteen',
+    14: 'Fourteen',
+    15: 'Fifteen',
+    16: 'Sixteen',
+    17: 'Seventeen',
+    18: 'Eighteen',
+    19: 'Nineteen',
+    20: 'Twenty',
+    30: 'Thirty',
+    40: 'Fourty',
+    50: 'Fifty',
+    60: 'Sixty',
+    70: 'Seventy',
+    80: 'Eighty',
+    90: 'Ninty',
+    100: 'Hundred',
+    1000: 'Thousand',
+    1000000: 'Million',
+    1000000000: 'Billion',
+    1000000000000: 'Trillion',
+    1000000000000000: 'Quadrillion'
+  }
+
+  if (num < 100 && teens.hasOwnProperty(num)) return teens[num];
+
+
+  const splitup = (''+num).split('');
+  console.log(splitup);
+  const digits = splitup.map((elem, i) => {
+    let numWord = '';
+    let digit = elem + '0'.repeat(splitup.length - 1 - i);
+    console.log(digit)
+    if (digit == 0) return numWord;
+    if (digit < 100 && teens.hasOwnProperty(digit)) {
+      numWord += teens[digit];
+    }
+    else if (digit >= 100 && teens.hasOwnProperty(digit)) {
+      numWord += 'One' + teens[digit];
+    }
+    else if (!teens.hasOwnProperty(digit)) {
+      numWord += teens[digit.charAt(0)];
+      digit = 1 + digit.slice(1);
+      numWord += teens[digit];
+    }
+    return numWord
+  })
+
+  return digits.join('');
 
 }
+
+console.log(numToWords(586428));
 
 module.exports = numToWords;
