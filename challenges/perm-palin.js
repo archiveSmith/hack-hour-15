51803fe9af6efd0c539fb3c84bc7317eb7cf4e3f
ibramos <i.am.ian.ramos@gmail.com>
2017-07-07 10:03:28 -0700

    finished perm-palin

--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -10,7 +10,21 @@
  */
 
 function permPalin(str) {
-	
+    if(typeof str !== 'string') return false
+    if(str.length === 1) return true
+    const letterCount = {};
+    let letter;
+    let sum = 0;
+    for (let i = 0; i < str.length; i++) {
+        letter = str[i];
+        letterCount[letter] = letterCount[letter] || 0;
+        letterCount[letter]++;
+    }
+    for (let letter in letterCount) {
+        sum += letterCount[letter] % 2;
+    }
+
+    return sum < 2;
 }
 
 module.exports = permPalin;
