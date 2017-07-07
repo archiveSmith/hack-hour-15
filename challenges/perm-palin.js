0352115087af3bc992472622eea3f74a06317054
Jerry Jong <jerryjong01@gmail.com>
2017-07-07 10:02:43 -0700

    perm-palin

--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -10,7 +10,15 @@
  */
 
 function permPalin(str) {
-	
+  let pal = str.split('').sort();
+
+  for (let i = 0; i < pal.length - 1; i++) {
+    if (pal[i] === pal[i + 1]) {
+      pal.splice(i, 2);
+      i -= 1;
+    }
+  }
+  return pal.length <= 1;
 }
 
 module.exports = permPalin;
