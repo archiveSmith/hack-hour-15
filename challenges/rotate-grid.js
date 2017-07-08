96d7e68bed62887c6776a9bcce7d3802384b6753
Brian Schmalz <bschmalz@Brians-MacBook-Air.local>
2017-07-08 09:36:53 -0700

    Rotated the grid. Didn't do it in place though.

--- challenges/rotate-grid.js
+++ challenges/rotate-grid.js
@@ -16,8 +16,21 @@
  * BONUS: Do this in place
  */
 
-function rotateGrid(grid, n) {
+ // Determine length of array and subArray
+ // Logic for flipping: 
+ // first array becomes last index
+ // second array becomes second index
 
+function rotateGrid(grid, n) {
+	if (n === 0) return [];
+	let result = [...Array(n).keys()].map(i => Array(n));
+	for (let x = 0; x < n; x++) {
+		let subArray = grid[x];
+		for (let y = 0; y < n; y++) {
+			result[y][n - 1 - x] = subArray[y];
+		}
+	}
+	return result; 
 }
 
 module.exports = rotateGrid;
