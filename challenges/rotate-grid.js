210e6c8d18b9e56cb3f3c40f928c4ac0e3a66fb9
Brett Beekley <brettbeekley@gmail.com>
2017-07-08 09:28:35 -0700

    Finished hack hour

--- challenges/rotate-grid.js
+++ challenges/rotate-grid.js
@@ -16,8 +16,34 @@
  * BONUS: Do this in place
  */
 
-function rotateGrid(grid, n) {
+// Input
+  // 2d, NxN array
+  // Number of times to rotate 90 degrees clockwise
+// Ouptut
+  // The input array rotated n * 90 degrees clockwise
 
+function rotateGrid(grid, n) {
+  // Rotate the grid n times
+  for (let i = 0; i < n; i += 1) {
+    
+    // Rotate the grid
+    
+    // Create a new outer array
+    grid = grid[0].map((col, i) => {
+      
+      // Where each element is an array
+      return grid.map((row) => {
+        
+        // Containing the values of a row from bottom to top
+        return row[row.length - i - 1];
+      });
+    });
+  }
+  
+  return grid;
 }
+
+// Test cases
+//console.log(rotateGrid([[1, 2, 3],[4, 5, 6],[7, 8, 9]], 3)); 
 
 module.exports = rotateGrid;
