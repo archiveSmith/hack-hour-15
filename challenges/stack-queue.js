812cc483b9a126a722aea4b49305889cc6f1f2b6
ibramos <i.am.ian.ramos@gmail.com>
2017-07-10 09:59:51 -0700

    finished stack queue

--- challenges/stack-queue.js
+++ challenges/stack-queue.js
@@ -4,8 +4,22 @@
 
 
 function Stack() {
-
+  this.storage = [];
+  this.index = 0;
 }
+
+Stack.prototype.addStack = function(value) {
+  this.storage[this.index] = value;
+  this.index++;
+  return value;
+}
+
+Stack.prototype.removeStack = function() {
+  let removed = this.storage[this.index-1];
+  delete this.storage[this.index--];
+  return removed;
+}
+
 
 
 /**
@@ -14,7 +28,26 @@ function Stack() {
 
 
 function Queue() {
+  this.incomingStack = new Stack();
+  this.goingOutStack = new Stack();
+}
 
+Queue.prototype.addQueue = function(value) {
+  this.incomingStack.push(value)
+  return value;
+}
+
+Queue.prototype.removeQueue = function() {
+  if(this.goingOutStack.index === 0) {
+    if(this.incomingStack.index === 0) return undefined;
+    while(this.incomingStack.index > 0) {
+      this.goingOutStack.push(this.incomingStack.pop())  		
+    }
+  }
+  return this.goingOutStack.pop();
 }
 
 module.exports = {Stack: Stack, Queue: Queue};
+
+
+
