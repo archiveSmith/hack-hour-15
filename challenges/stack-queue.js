4298bb4cfb0db94b3b12f37ad58c6ee97701d2f5
OnuPete <pcr137@case.edu>
2017-07-10 18:26:09 -0700

    stacked up

--- challenges/stack-queue.js
+++ challenges/stack-queue.js
@@ -4,6 +4,16 @@
 
 
 function Stack() {
+  this.stack = [];
+  this.push = (elem) => {
+      this.stack.push(elem);
+    }
+  this.pop = () => {
+      return this.stack.pop();
+    }
+  this.peek = () => {
+      return this.stack[stack.length - 1];
+    }
 
 }
 
@@ -12,9 +22,46 @@ function Stack() {
 * Queue Class
 */
 
+// const stack = new Stack();
+// stack.push(3);
+// stack.push(4);
+// stack.push(1);
+// stack.pop()
+
+
 
 function Queue() {
+  this.stack1 = new Stack();
+  this.stack2 = new Stack();
+  this.queue = this.stack1.stack.length ? this.stack1.stack : this.stack2.stack;
+  this.pop = () => {
+    const [stack1, stack2] = [this.stack1, this.stack2];
+    while (stack1.stack.length) {
+      stack2.push(stack1.pop());
+    }
 
+    return stack2.pop();
+  }
+  this.push = (elem) => {
+    if (!this.stack2.stack.length) {
+      this.stack1.push(elem);
+      return;
+    }
+    const [stack1, stack2] = [this.stack1, this.stack2];
+
+    while (stack2.stack.length > 0) {
+      stack1.push(stack2.pop());
+    }
+    stack1.push(elem);
+  }
 }
+// const queue = new Queue();
+// queue.push(3);
+// queue.push(5);
+// queue.push(9)
+// console.log(queue.pop());
+
+
+// console.log(queue);
 
 module.exports = {Stack: Stack, Queue: Queue};
