d20fc556f6df23469608a5062498f37278f48a25
OnuPete <pcr137@case.edu>
2017-07-11 09:32:42 -0700

    gcd no negs

--- challenges/gcd.js
+++ challenges/gcd.js
@@ -8,7 +8,17 @@
  */
 
 function gcd(a, b) {
+  const [bigger, smaller] = a > b? [a, b] : [b, a];
+  let gdc = 0;
+  for (let i = 0; i < bigger; i++) {
+    if (bigger % i === 0 && smaller % i === 0 && i > gdc) {
+      gdc = i
+    }
+  }
+  return gdc;
 
 }
+
+// console.log(gcd(64,24))
 
 module.exports = gcd;
