a8c5cc7d198f3490ea522ec31b77c89091987181
Christopher Dobler <chrisdobler@gmail.com>
2017-07-10 20:20:36 -0700

    Adds Stack Queue

--- challenges/stack-queue.js
+++ challenges/stack-queue.js
@@ -4,7 +4,14 @@
 
 
 function Stack() {
+  const storage = [];
+  push(item) {
+    storage.push(item);
+  }
 
+  pop() {
+    return storage.pop();
+  }
 }
 
 
@@ -14,7 +21,22 @@ function Stack() {
 
 
 function Queue() {
+  const input = [];
+  const output = [];
+  enqueue(item) {
+    input.push(item);
+  }
 
+  dequeue() {
+    if (output.count == 0)
+    {
+      while (input.count != 0)
+      {
+        output.push(input.pop());
+      }
+    }
+    return output.pop();
+  }
 }
 
 module.exports = {Stack: Stack, Queue: Queue};
