c663518e94cde51f9e0446acd2c87265135e306c
mm0nr0e <monroe.mirand@gmail.com>
2017-07-13 09:42:37 -0700

    hackhour

--- challenges/max-stack.js
+++ challenges/max-stack.js
@@ -8,6 +8,29 @@
 
 function Stack() {
   // body...
+  this.contents = {};
+  this.length = 0;
+}
+
+Stack.prototype.push = function(item) {
+  this.contents[this.length] = item;
+  this.length += 1;
+  return this.length;
+}
+
+Stack.prototype.pop = function() {
+  const toReturn = this.contents[--this.length];
+  delete this.contents[this.length];
+  return toReturn;
+}
+
+Stack.prototype.getMax = function() {
+  let max = -Infinity;
+  for(let item in this.contents) {
+    console.log(item);
+    max = Math.max(max, this.contents[item]);
+  }
+  return max;
 }
 
 module.exports = Stack;
