1d5ee48a773f9dabee998b67df374c2b7c2231f5
Melissa Schwartz <melissjs@gmail.com>
2017-07-13 09:39:05 -0700

    wrking

--- challenges/max-stack.js
+++ challenges/max-stack.js
@@ -8,6 +8,43 @@
 
 function Stack() {
   // body...
+this.count = 0;
+this.storage = {}
 }
 
+Stack.prototype.push = function(value) {
+    this.storage[this.count] = value;
+    this.count++;
+    return this.count;
+}
+  
+Stack.prototype.pop = function() {
+    if (this.count === 0) {
+        return undefined;
+    }
+
+    this.count--;
+    var result = this.storage[this.count];
+    delete this.storage[this.count];
+    return result;
+}
+
+Stack.prototype.getMax = function() {
+  let max = -Infinity;
+  for (prop in this.storage){
+    if (this.storage[prop]>max){
+      max = this.storage[prop]
+    }
+  }
+  return max;
+}
+let myStack = new Stack;
+myStack.push(5);
+myStack.push(2);
+myStack.push(7);
+
+console.log(myStack);
+console.log(myStack.push(9));
+console.log(myStack.pop());
+console.log(myStack.getMax());
 module.exports = Stack;
