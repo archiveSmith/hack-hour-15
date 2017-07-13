0ee4f36e64f8f8aa99335eb1bfb3f615f35829f3
pennwu1 <phw.dental@gmail.com>
2017-07-13 09:38:43 -0700

    complete max-stack.js

--- challenges/max-stack.js
+++ challenges/max-stack.js
@@ -6,8 +6,43 @@
  * BONUS: The getMax method should retrieve the maximum value from the stack in O(1) time.
  */
 
+// Push
+// Pop
+// getMax
+  // retrieves the maximum value from the stack
+
 function Stack() {
-  // body...
+  this.data = {};
+  this.length = 0;
 }
+
+Stack.prototype.push = function(value) {
+  this.data[this.length] = value;
+  this.length++;
+};
+
+Stack.prototype.pop = function() {
+  if (this.length > 0) {
+    const poppedEle = this.data[this.length - 1];
+    this.length--;
+    return poppedEle;
+  }
+  return undefined;
+};
+
+Stack.prototype.getMax = function() {
+  const values = Object.values(this.data);
+  return Math.max(...values);
+};
+
+// const testStack = new Stack;
+// testStack.push(2);
+// testStack.push(5);
+// testStack.push(10);
+// testStack.getMax();
+// testStack.pop();
+// testStack.pop();
+// testStack.pop();
+// testStack.pop();
 
 module.exports = Stack;
