ae68371cbb3fc643d1618f16fc9b87263f1ef588
icrustandi <icrustandi@gmail.com>
2017-07-13 09:38:34 -0700

    Stack max attempt

--- challenges/valid-bst.js
+++ challenges/valid-bst.js
@@ -15,18 +15,26 @@ function BinaryTree(val) {
 function validBST(tree) {
   if (traverse(tree) === false) return false;
   return true;
+
 }
 
 function traverse(tree) {
   if (!tree) return;
-  if (tree.left) {
-    if (tree.left.value > tree.value) return false;
-  }
-  if (tree.right) {
-    if (tree.right.value < tree.value) return false;
-  }
+  if (tree.left && tree.left.value > tree.value) return false;
+  if (tree.right && (tree.right.value < tree.value)) return false;
   if (traverse(tree.left) !== undefined) return traverse(tree.left);
   if (traverse(tree.right) !== undefined) return traverse(tree.right);
 }
+
+tree = new BinaryTree(11);
+tree.left = new BinaryTree(9);
+tree1 = tree.left
+tree1.left = new BinaryTree(8);
+tree1.right = new BinaryTree(10);
+tree.right = new BinaryTree(13);
+tree2 = tree.right;
+tree2.left = new BinaryTree(11);
+tree2.right = new BinaryTree(15);
+console.log(validBST(tree));
 
 module.exports = {BinaryTree: BinaryTree, validBST: validBST};
