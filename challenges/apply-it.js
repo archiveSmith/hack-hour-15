8258f44e992bec4be04e9239a276cc0621b6ef6a
Christopher Dobler <chrisdobler@gmail.com>
2017-07-14 10:26:28 -0700

    Adds Apply It

--- challenges/apply-it.js
+++ challenges/apply-it.js
@@ -25,8 +25,21 @@
  *  DO NOT USE THE BUILT IN APPLY METHOD OR THE SPREAD OPERATOR
  */
 
-function applyIt(func, args) {
-
+function applyIt(fn, args) {    
+    switch (args ? args.length : 0) {
+        case 0:
+            return context ? fn.call(this) : fn();
+        case 1:
+            return context ? fn.call(this, args[0]) : fn(args[0]);
+        case 2:
+            return context ? fn.call(this, args[0], args[1]) : fn(args[0], args[1]);
+        case 3:
+            return context ? fn.call(this, args[0], args[1], args[2]) : fn(args[0], args[1], args[2]);
+        case 4:
+            return context ? fn.call(this, args[0], args[1], args[2], args[3]) : fn(args[0], args[1], args[2], args[3]);
+        case 5:
+            return context ? fn.call(this, args[0], args[1], args[2], args[3], args[4]) : fn(args[0], args[1], args[2], args[3], args[4]);
+    }  
 }
 
 module.exports = applyIt;
