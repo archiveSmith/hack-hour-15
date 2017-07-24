379fe2398e7cee9f95baf187c49be253d83f0dc3
ibramos <i.am.ian.ramos@gmail.com>
2017-07-24 09:58:16 -0700

    unfished countTwos

--- challenges/count-twos.js
+++ challenges/count-twos.js
@@ -8,7 +8,22 @@
 
 
 function countTwos(num) {
-
+    if(typeof num !== 'number') return 'Invalid argument';
+    let counter = 0;
+    let i;
+    let j;
+    let temp;
+    for (i=0; i<num; i++) {
+        if(i.toString().includes('2')) {
+          temp = i.toString().split('')
+          for(j=0; j<i.length; j++) {
+            if(temp[j] === '2') counter++
+          }
+        }
+    }
+    return counter;
 }
+
+
 
 module.exports = countTwos;
