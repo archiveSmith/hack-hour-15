d714bd6944ac7e0d5dea3c4a1cb0df3072926894
Jeff Yamamoto <jeffmagician@gmail.com>
2017-07-26 10:29:05 -0700

    Try to implement superbalance

--- challenges/superbalanced.js
+++ challenges/superbalanced.js
@@ -14,7 +14,45 @@ function BinaryTree(value) {
 }
 
 function superbalanced(tree) {
+  // if tree is null or undefined, return true
+  if(!tree) return true
+  
+  // define helper function "checkSuperBalanced"
+  function checkSuperBalanced(branch, depth){
+    // in: tree, current depth
+    // out: {balanced:, maxheight:}
+    if(!branch) return { balanced: true, maxheight: depth - 1}
 
+    // check if each branch is superbalanced by calling checkSuperBalanced(branch, depth + 1)
+    // assign result of checkSuperBalanced to variable
+    var leftResult = checkSuperBalanced(branch.left, depth + 1)
+    var rightResult = checkSuperBalanced(branch.right, depth + 1)
+    // if one of them is not balanced, return false for "balanced"
+    var heightDiff = leftResult.maxheight - rightResult.maxheight
+    var balanced = leftResult.balanced && rightResult.balanced && Math.abs(heightDiff) <= 1
+    return {
+      balanced: balanced,
+      maxheight: depth
+    }
+  }
+  var result = checkSuperBalanced(tree, 1)
+  return result.balanced
 }
+
+// var bst
+// console.log(superbalanced(bst)) // true
+// bst = new BinaryTree(8)
+// console.log(superbalanced(bst)) // true
+// bst.left = new BinaryTree(4)
+// bst.left.left = new BinaryTree(2) 
+// console.log(superbalanced(bst)) // false
+// bst.left.left.right = new BinaryTree(3 )
+// bst.left.right = new BinaryTree(5) 
+// console.log(superbalanced(bst)) // false
+// bst.right = new BinaryTree(12)
+// bst.right.left = new BinaryTree(11)
+// console.log(superbalanced(bst)) // true
+// bst.right.right = new BinaryTree(14)
+
 
 module.exports = {BinaryTree: BinaryTree, superbalanced: superbalanced};
