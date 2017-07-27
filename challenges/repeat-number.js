d824385175bb9ef96a9500f1b4af8fc7ccada0f7
mm0nr0e <monroe.mirand@gmail.com>
2017-07-27 09:28:41 -0700

    hackhour

--- challenges/repeat-number.js
+++ challenges/repeat-number.js
@@ -11,7 +11,23 @@
  */
 
 function repeatNumbers(array) {
+  // if you know all in range, you can do the maths?
+  // 1...n where n will === array.length-1
+  // ex [1, 2, 5, 4, 2, 3]
+  // you would have an expected sum variable that could be reasigned/
+  // calculate expected sum and then calculate actual sum then get difference
+  // create a for loop and go thru and increment one sum with array[i] and one with (i)
+  // return difference of two sums
 
+  let expectedSum = 0;
+  let actualSum = 0;
+
+  for (i = 0; i < array.length; i += 1) {
+    expectedSum += i;
+    actualSum += array[i];
+  }
+  
+  return actualSum - expectedSum;
 }
 
 module.exports = repeatNumbers;
