e172f8095da7abde696c813cbaa8b4b95e5e840f
Jeff Yamamoto <jeffmagician@gmail.com>
2017-07-27 09:30:38 -0700

    Implement repeat number

--- challenges/repeat-number.js
+++ challenges/repeat-number.js
@@ -11,7 +11,20 @@
  */
 
 function repeatNumbers(array) {
-
+  if (!array || array.length === 0) return undefined
+  const dict = {}
+  for (let i = 0; i < array.length; i++){
+    if(!dict[array[i]]){
+      dict[array[i]] = {}
+      continue
+    }
+    return array[i] 
+  }
+  return undefined
 }
+// console.log(repeatNumbers([])) // undefined
+// console.log(repeatNumbers([1])) // undefined
+// console.log(repeatNumbers([1, 2])) // undefined
+// console.log(repeatNumbers([2, 1, 2])) // 2
 
 module.exports = repeatNumbers;
