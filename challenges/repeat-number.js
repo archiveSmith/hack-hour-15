e03d1f8650bcf8b9b5da4b9f296129a9d5b96962
Garrett Levy <garrettlr@gmail.com>
2017-07-27 09:43:00 -0700

    forcing tests because solns are bad

--- challenges/repeat-number.js
+++ challenges/repeat-number.js
@@ -26,26 +26,54 @@
 //   return num;
 // }
   //meets bonus criteria;
+// function repeatNumbers(array) {
+//     let num = Number;
+//
+//   for (let i = 0; i < array.length; i++) {
+//       //check to see if the value at array[number, positive] is > 0
+//     if (array[Math.abs(array[i])] >= 0) {
+//       //if yes, make it negative (why?);
+//       array[Math.abs(array[i])] = -array[Math.abs(array[i])];
+//     } else {
+//       //if not, thats the repeating number.
+//       num = Math.abs(array[i]);
+//     }
+//
+//     // console.log('iteration: ', i);
+//     // console.log('array: ', array);
+//     // console.log('weird math: ', array[Math.abs(array[i])]);
+//     // console.log('num: ', num);
+//
+//   }
+//   return num;
+// }
+
+// function repeatNumbers(array) {
+//   var result = 0;
+//
+//   for (var i = 0; i < array.length; i ++) {
+//     result = result ^ i ^ array[i];
+//     // console.log(result);
+//   }
+//
+//   return result;
+// }
+
+// O(n) time, O(1) space
 function repeatNumbers(array) {
-    let num = Number;
+  var expected = ( ( array.length - 1 ) * array.length ) / 2;
+  console.log('expected: ', expected);
+  var actual = 0;
 
-  for (let i = 0; i < array.length; i++) {
-
-    if (array[Math.abs(array[i])] >= 0) {
-      array[Math.abs(array[i])] = -array[Math.abs(array[i])];
-    } else {
-      num = Math.abs(array[i]);
-    }
-
-    // console.log('iteration: ', i);
-    // console.log('array: ', array);
-    // console.log('weird math: ', array[Math.abs(array[i])]);
-    // console.log('num: ', num);
-
+  for (var i = 0; i < array.length; i ++) {
+      actual += array[i];
+      console.log(actual);
   }
-  return num;
+
+  return actual - expected;
 }
 
-// console.log(repeatNumbers([16,1,2,16,0,5]));
+
+console.log("final: ", repeatNumbers([16,1,2,16,5,0]));
 
 module.exports = repeatNumbers;
