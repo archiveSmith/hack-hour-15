ac943e44c0cc406e5065e96886a81eac938dadd8
KseniaAK <ksenia.kozh@gmail.com>
2017-07-28 09:50:06 -0700

    merge ranges

--- challenges/merge-ranges.js
+++ challenges/merge-ranges.js
@@ -11,7 +11,20 @@
 
 
 function mergeRanges(array) {
-
+  if (!Array.isArray(array)) return;
+  array.sort((a, b) => a[0]> b[0]);
+  console.log(array);
+  const result = [];
+  for (let i = 0; i < array.length; ) {
+    if (array[i][1] >= array[i + 1][0]) {
+      result.push([array[i][0], array[i + 1][1]]);
+      i += 2;
+    } else {
+      result.push(array[i]);
+      i++;
+    }
+  }
+  return result;
 }
 
 module.exports = mergeRanges;
