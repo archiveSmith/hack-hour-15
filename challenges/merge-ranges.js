ee87ebc3e93b8f31457b1f3c4505f8792614834d
ibramos <i.am.ian.ramos@gmail.com>
2017-07-28 09:44:49 -0700

    missing one last logic to complete merge-ranges

--- challenges/merge-ranges.js
+++ challenges/merge-ranges.js
@@ -11,7 +11,41 @@
 
 
 function mergeRanges(array) {
+    let i;
+    let tempNum;
+    let tempArray = [];
+    const mergedTimes = [];
+    const newArray = 
+        array.reduce(function(a, b) {
+          return a.concat(b);
+        }, [])
+    const sortedArray = 
+        newArray.sort(function(c, d) {
+           return c - d;
+        });
+    console.log('newArray: ', newArray);
+    for(i=0; i<newArray.length; i++) {
+          tempNum = newArray[i]
+          console.log('tempNum: ', tempNum)
+          if(tempNum <= newArray[i] && !tempArray.includes(newArray[i])){
+            if(newArray[i] >= sortedArray[i]){
+              tempArray.push(tempNum)
+              console.log('tempArray: ', tempArray)
+            } else {
+              tempNum = sortedArray[i]
+              }
+            if(tempArray.length === 2) {
+              mergedTimes.push(tempArray)
+              console.log('mergedTimes: ', mergedTimes)
+              tempArray = [];
+            }
+          } 
+        }      
+
+        
+    return mergedTimes
 
 }
+
 
 module.exports = mergeRanges;
