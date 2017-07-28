337a214eaaad65efb66f2d01fa1a73f768c1ca09
OnuPete <pcr137@case.edu>
2017-07-28 12:51:33 -0700

    merging times

--- challenges/merge-ranges.js
+++ challenges/merge-ranges.js
@@ -11,7 +11,25 @@
 
 
 function mergeRanges(array) {
-
+  array.sort((a, b) => {
+    return a[0] - b[0];
+  })
+  return array.reduce((acc, elem) => {
+    let times = [];
+    let temp = acc.pop();
+    if (temp[1] >= elem[0]) {
+      times.push(temp[0]);
+      times.push(elem[1]);
+    } else {
+      acc.push(temp);
+      times = elem;
+    }
+    acc.push(times);
+    return acc;
+  }, [array.shift()]);
 }
+
+var times = [[0, 1], [3, 5], [4, 8], [10, 12], [9, 10]];
+console.log(mergeRanges(times));
 
 module.exports = mergeRanges;
