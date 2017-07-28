42069f7db578eb224398e398815c81b90d104c2c
Brian Schmalz <bschmalz@Brians-MacBook-Air.local>
2017-07-28 09:38:03 -0700

    stuff

--- challenges/merge-ranges.js
+++ challenges/merge-ranges.js
@@ -11,7 +11,22 @@
 
 
 function mergeRanges(array) {
-
+  let sorted = times.sort((a,b) => a[0] - b[0]);
+  let result = []; 
+  result.push(sorted.shift()); 
+  let index = 0; 
+  sorted.forEach(item => {
+    if (item[0] <= result[index][1]) {
+      result[index][1] = item[1]; 
+    } else {
+      result.push(item); 
+      index++; 
+    }
+  });
+  return result; 
 }
+
+
+
 
 module.exports = mergeRanges;
