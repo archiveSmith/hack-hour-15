6ed870e4dd5966de60b6063c721eb5fa50b683f4
Janelle Wong <janelle.wg@gmail.com>
2017-07-29 09:38:58 -0700

    added null arg

--- challenges/linked-list-cycle.js
+++ challenges/linked-list-cycle.js
@@ -39,7 +39,7 @@ var Node = function(value) {
 //return true/false
 
 function hasCycle(head) {
-  if(!head.value) return false;
+  if(!head.value || head.value === null) return false;
   //set two pointers
   let prev = head;
   let curr = head.next; 
