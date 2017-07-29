44e8cfc41df21bf4fb7b3cb598a8e94ed97f9c46
OnuPete <pcr137@case.edu>
2017-07-29 09:40:23 -0700

    bicycle

--- challenges/linked-list-cycle.js
+++ challenges/linked-list-cycle.js
@@ -33,7 +33,24 @@ var Node = function(value) {
 }
 
 function hasCycle(head) {
-
+  if (!(head instanceof Node)) return 'invalid input';
+  let slow = head;
+  let fast = head.next;
+  while(slow && fast) {
+    if (slow === fast) return true;
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+  return false;
 }
+
+// var node1 = new Node('1');
+// var node2 = node1.next = new Node('2');
+// var node3 = node2.next = new Node('3');
+// var node4 = node3.next = new Node('4');
+// var node5 = node4.next = new Node('5');
+// console.log(hasCycle(node1)); // => false
+// node5.next = node2;
+// console.log(hasCycle(node1));
 
 module.exports = {Node: Node, hasCycle: hasCycle}
