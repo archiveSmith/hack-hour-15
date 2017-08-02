65491b60ac524f55ff0c787ba4302caa64c6e4bd
Brian Schmalz <bschmalz@Brians-MacBook-Air.local>
2017-08-02 09:35:47 -0700

    Eh.

--- challenges/anagrams.js
+++ challenges/anagrams.js
@@ -13,7 +13,23 @@
   */
 
 function anagrams(string) {
+	if (word.length < 2) {
+        return [word];
+    } else {
+        var allanswers = [];
+        for (var i = 0; i < word.length; i++) {
+            var letter = word[i];
+            var shorterword = word.substr(0, i) + word.substr(i + 1, word.length - 1);
+            var shortwordarray = allanagrams(shorterword);
+            for (var j = 0; j < shortwordarray.length; j++) {
+                allanswers.push(letter + shortwordarray[j]);
+            }
+        }
+        return allanswers;
+    }
 
 }
+
+
 
 module.exports = anagrams;
