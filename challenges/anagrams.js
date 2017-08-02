35f86b1a5a53ac289be1680ca56d48e25093d4ad
OnuPete <pcr137@case.edu>
2017-08-02 09:41:50 -0700

    ana grama

--- challenges/anagrams.js
+++ challenges/anagrams.js
@@ -13,7 +13,27 @@
   */
 
 function anagrams(string) {
+  if (typeof string !== 'string') return 'invalid input';
+  if (string.length < 2) return string;
+  const permutations = [];
+	permutations.push(string[0]);
+	string = string.slice(1);
+	while (string.length > 0) {
+		const toPerm = [];
+		while (permutations.length > 0) {
+			let chunk = permutations.pop();
+			for (let i = 0; i <= chunk.length; i += 1) {
+				toPerm.push(chunk.slice(0, i) + string[0] + chunk.slice(i));
+			}
+		}
+		permutations.push(...toPerm);
+		string = string.slice(1);
+	}
 
+	return permutations;
 }
+
+var result = anagrams('abc');
+console.log(result);
 
 module.exports = anagrams;
