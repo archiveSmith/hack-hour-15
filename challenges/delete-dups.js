540236fc98a6700c16bdf996d065459816fbd4f3
Melissa Schwartz <melissjs@gmail.com>
2017-08-03 09:46:33 -0700

    not working

--- challenges/delete-dups.js
+++ challenges/delete-dups.js
@@ -10,10 +10,57 @@
  * How would you solve this problem if a temporary buffer is not allowed?
  */
 
+//list obj
+function Node(val) {
+  this.value = val;
+  this.next = null;
+  this.prev = null;
+  this.head = null;
+  this.tail = null;
+}
 
+//print list
+Node.prototype.printLL= (list) => {
+  let head = list;
+  let str = "";
+  while (head != null){
+      str = str + head.value + '->';
+      console.log(str);
+      head = head.next;
+  }
+}
+
+// add to list
+Node.prototype.addToList = (val) => {
+if (!this.head) {
+    this.head = this.tail = new Node(val);
+} else {
+    this.tail.next = this.head.next = new Node(val);
+    this.tail = this.tail.next;
+}
+}
 
 function deleteDups(head) {
+    // let curr = head;
+    // let buffer = [];
+    // while (curr!=null) {
+    //     buffer.forEach((bufEl)=>{
+    //         if (curr.value === bufEl){
+
+    //         }
+    //     })
+    //     buffer.push(curr.value);
+    //     curr = curr.next
+    // }
+    // console.log(buffer)
 
 }
 
 module.exports = deleteDups;
+
+node1 = new Node(1);
+node2 = node1.next = new Node(2);
+node3 = node2.next = new Node(3);
+
+// console.log(node1.printLL(node1))
+// console.log(deleteDups(node1))
