8b5d06fa12cf2291a03f0ad24cb253721b0f3a35
Brett Beekley <brettbeekley@gmail.com>
2017-08-04 09:16:04 -0700

    This feels more like an event listener than emitter

--- challenges/event-emitter.js
+++ challenges/event-emitter.js
@@ -22,15 +22,35 @@
  */
 
 function EventEmitter() {
-
+  // Events will be stored as keys
+  // Functions will be stored in an array as the value
+  this.functionStore = {};
 }
 
+// Define a function that should run on an event
 EventEmitter.prototype.on = function(funcName, func) {
-
+  // Add the function to the array at the key funcName
+  if (!this.functionStore[funcName]) this.functionStore[funcName] = [];
+  this.functionStore[funcName].push(func);
 };
 
+// Run all functions attached to the event with given args
 EventEmitter.prototype.trigger = function(funcName, ...args) {
-
+  // If no functions defined, return
+  if (!this.functionStore[funcName]) return;
+  // Iterate through functions for the event
+  this.functionStore[funcName].forEach(func => func(...args));
 };
+
+// Test case
+//var instance = new EventEmitter();
+//var counter = 0;
+//instance.on('increment', function() {
+// counter++;
+//}); // counter should be 0
+//instance.trigger('increment'); // counter should be 1
+//console.log(counter, 1);
+//instance.trigger('increment'); // counter should be 2
+//console.log(counter, 2);
 
 module.exports = EventEmitter;
