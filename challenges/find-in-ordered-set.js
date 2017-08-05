a01042594024236ef937276be302a9fd1fff3293
Garrett Levy <garrettlr@gmail.com>
2017-08-05 09:39:54 -0700

    bin search

--- challenges/find-in-ordered-set.js
+++ challenges/find-in-ordered-set.js
@@ -11,8 +11,23 @@ findInOrderedSet(nums, 2);  -> false
 
 
 function findInOrderedSet(arr, target) {
-
+  // const set = new Set(arr);
+  // const size = set.size;
+  // set.add(target);
+  // return set.size === size;
+  return findMid(0, arr.length - 1, target, arr);
 }
+
+function findMid(min, max, target, arr) {
+  if (max < min) return false;
+  const mid = (max + min) / 2;
+  if (target === arr[mid]) return true;
+  if (target < arr[mid]) return findMid(min, (mid - 1), target, arr);
+
+  return findMid((mid + 1), max, target, arr)
+}
+
+
 
 
 module.exports = findInOrderedSet;
