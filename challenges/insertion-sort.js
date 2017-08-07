31aafc0c7f7c0dafe5b23a264809356bac8fc050
Brian Schmalz <bschmalz@Brians-MacBook-Air.local>
2017-08-07 09:34:30 -0700

    insertion sort

--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -5,7 +5,15 @@
 
 
 function insertionSort(array) {
-
+  for (let i=1; i<array.length; i++) {
+    if (array[i] < array[i-1]) {
+      let temp = array[i];
+      array[i] = array[i -1];
+      array[i - 1] = temp; 
+      array = insertionSort(array);
+    }
+  }
+  return array; 
 }
 
 module.exports = insertionSort;
