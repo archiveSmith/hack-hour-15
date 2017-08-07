c6bfed62c6d33cb03aa899bf19eb385102634193
Ian Ramos <i.am.ian.ramos@gmail.com>
2017-08-07 09:31:19 -0700

    finished insertion sort

--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -6,6 +6,26 @@
 
 function insertionSort(array) {
 
-}
+  if(!Array.isArray(array)) return 'Invalid argument'
+  if (array.length === 1) return array;
 
+  let element = array.shift();
+  console.log('element: ', element)
+  
+  console.log('array: ', array)
+  let result = insertionSort(array);
+  console.log('result rescursed: ', result)
+  
+  let i = result.length;
+  console.log('i: ', i)
+  
+  while (i >= 0 && element <= result[i-1]) i--;
+  console.log('i inside while: ', i)
+
+  result.splice(i, 0, element);
+  console.log('result inside while: ', result)
+  
+  return result;
+
+}
 module.exports = insertionSort;
