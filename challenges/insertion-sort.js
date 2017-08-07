e428a1fe6132f091b832c6b791cac8d1a50c0337
icrustandi <icrustandi@gmail.com>
2017-08-07 09:26:58 -0700

    Finish insertion sort

--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -5,7 +5,18 @@
 
 
 function insertionSort(array) {
-
+  let temp;
+  for (let i = 1; i < array.length; i += 1) {
+    if (array[i] < array[i - 1]) {
+      temp = array[i];
+      array[i] = array [i - 1];
+      array[i - 1] = temp;
+      insertionSort(array);
+    }
+  }
+  return array;
 }
+
+// console.log(insertionSort([6, 5, 3, 1, 8, 7, 2, 4]));
 
 module.exports = insertionSort;
