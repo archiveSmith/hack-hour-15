8c25f7b7bfa33a7e7a1562221f95c3f07e0d69b2
OnuPete <pcr137@case.edu>
2017-08-08 09:32:14 -0700

    unique from dups

--- challenges/unique-number.js
+++ challenges/unique-number.js
@@ -10,7 +10,35 @@
  *
  */
 function uniqueNumber(array) {
+  if (!Array.isArray(array)) return 'Invalid Input';
+  if (array.length < 1) return 'something';
+  if (array.length === 1) return array[0];
+
+  // if array is an array from 0-n
+  let n = (array.length + 1) / 2
+  let expected = 0;
+  while (n) {
+    expected += n + n;
+    n -= 1;
+  }
+  const actual = array.reduce((acc, elem) => {
+    acc += elem;
+    return acc;
+  })
+  return expected - actual;
+
+  // if array is an array of any numbers
+  const seen = array.reduce((acc, elem) => {
+    if (acc[elem]) acc[elem] += 1;
+    else acc[elem] = 1;
+    return acc;
+  }, {})
+  for (num in seen) {
+    if (seen[num] === 1) return Number(num);
+  }
 
 }
+
+console.log(uniqueNumber([1,2,1,3,3, 4, 5, 4, 2]))
 
 module.exports = uniqueNumber;
