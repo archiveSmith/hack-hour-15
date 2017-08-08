af8ec3ecd0ac168ba8d6e289f6e39981ae7d441b
Brett Beekley <brettbeekley@gmail.com>
2017-08-08 09:19:21 -0700

    O(n) space

--- challenges/unique-number.js
+++ challenges/unique-number.js
@@ -9,8 +9,26 @@
  * Complete the challenge in O(1) space
  *
  */
+
+// Input
+  // An array of numbers, where one number appears once, and all other numbers appear exactly twice
+// Output
+  // The number that appears once
+
 function uniqueNumber(array) {
 
+  // O(n) 
+  // Create a storage object
+  const storage = {};
+  // Iterate through the array
+    // If the number doesnt exist in storage, add it
+    // Else delete it
+  array.forEach(num => storage[num] ? delete storage[num] : storage[num] = true)
+  // Return the only number left
+  return Object.keys(storage)[0];
 }
+
+// Test
+console.log(uniqueNumber([1,2,1,3,3]),2);
 
 module.exports = uniqueNumber;
