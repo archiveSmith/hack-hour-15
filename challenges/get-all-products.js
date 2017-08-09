fad3d5642d947cca3efa204e37fe16864a9cf754
Brett Beekley <brettbeekley@gmail.com>
2017-08-09 09:23:56 -0700

    Brute forced

--- challenges/get-all-products.js
+++ challenges/get-all-products.js
@@ -9,8 +9,31 @@
  * do not use division, becuase zero might be in the array and you cannot divide by zero
  */
 
-function getAllProducts(array) {
+ // Input
+  // Array of integers
+// Output
+  // Array of integers, containing all possible products of the input numbers except one
 
+function getAllProducts(array) {
+  // Create an output var
+  const output = [];
+  // Iterate through the array
+  array.forEach((outer, i) => {
+    // Create a product var
+    let product = 1;
+    // Iterate through the array
+    array.forEach((inner, j) => {
+      // if the current inner element is not the outer element, multiply it to the product
+      if (j !== i) product *= inner;
+    });
+    // Push the product to the output
+    output.push(product);
+  });
+  // Return the filtered output
+  return output.filter((prod, i) => output.indexOf(prod) === i);
 }
+
+// Test
+// console.log(getAllProducts([1, 7, 3, 4]), [84, 12, 28, 21]);
 
 module.exports = getAllProducts;
