0b8a022fd77ee5dbbb34358d3239539e3e63d24a
pennwu1 <phw.dental@gmail.com>
2017-08-10 09:41:17 -0700

    complete doubly-linked-list.js

--- challenges/doubly-linked-list.js
+++ challenges/doubly-linked-list.js
@@ -17,14 +17,43 @@ function Node(val) {
 Adds a node to the end of the list
  */
 LinkedList.prototype.add = function(val) {
-  
+  let currNode = new Node(val);
+  if (!this.head) {
+    this.head = currNode;
+    this.tail = currNode;
+  } else {
+    currNode.prev = this.tail;
+    this.tail.next = currNode;
+    this.tail = currNode;
+  }
 };
 
 /*
 Removes the first node with the inputted value
  */
 LinkedList.prototype.remove = function(val) {
+  // Find the first instance of node w/ inputted value
+  let currNode = this.head;
+  while (currNode) {
+    if (currNode.val === val) {
+      // console.log('this conditional is hit');
+      currNode.prev.next = currNode.next;
+      return currNode;
+    } else {
+      currNode = currNode.next;
+    }
+  }
+  return false;
+
+  // Connect the previous node w/ the next node
   
 };
+
+// let listOf3 = new LinkedList;
+// listOf3.add(1);
+// listOf3.add(2);
+// listOf3.add(3);
+
+// console.log('remove', listOf3.remove(3));
 
 module.exports = LinkedList;
