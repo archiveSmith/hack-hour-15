371fe7a5e5490f736c134e62ac284ab812071e6c
Christopher Dobler <chrisdobler@gmail.com>
2017-08-10 11:30:26 -0700

    Adds doubly linkedlist

--- challenges/doubly-linked-list.js
+++ challenges/doubly-linked-list.js
@@ -17,14 +17,32 @@ function Node(val) {
 Adds a node to the end of the list
  */
 LinkedList.prototype.add = function(val) {
+  const newNode = new Node(val);
+
+  if(this.head === null) this.head = newNode;
   
+  if (this.tail) this.tail.next = newNode;
+  newNode.prev = this.tail;
+
+  this.tail = newNode;
 };
 
 /*
 Removes the first node with the inputted value
  */
 LinkedList.prototype.remove = function(val) {
-  
+  let cur = this.head;
+  while (cur.next) {
+    if (cur.val === val) {
+      cur.next.prev = cur.prev;
+      cur.prev.next = cur.next;
+      break;
+    } else {
+      cur = cur.next;
+    }
+  }
 };
+
+const list = new LinkedList();
 
 module.exports = LinkedList;
