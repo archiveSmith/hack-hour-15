b870aba02d2c0c0e5ea37948f0196b8acb71b850
Brian Schmalz <bschmalz@Brians-MacBook-Air.local>
2017-08-10 09:23:28 -0700

    Couple hack hours.

--- challenges/doubly-linked-list.js
+++ challenges/doubly-linked-list.js
@@ -2,7 +2,7 @@
 Create a doubly linked list with an add and remove method
  */
 
-function LinkedList() {
+function LinkedList(val) {
   this.head = null;
   this.tail = null;
 }
@@ -17,14 +17,37 @@ function Node(val) {
 Adds a node to the end of the list
  */
 LinkedList.prototype.add = function(val) {
-  
+  let newNode = new Node(val); 
+  if (!this.head) {
+  	this.head = newNode; 
+  	return; 
+  }
+  let current; 
+  if (this.tail) current = this.tail; 
+  else current = this.head; 
+  newNode.prev = current; 
+  current.next =  newNode; 
+  this.tail = newNode;
 };
 
 /*
 Removes the first node with the inputted value
  */
 LinkedList.prototype.remove = function(val) {
-  
+  if (!this.head) return; 
+  let current = this.head; 
+  if (current.val === val) this.head = this.head.next; 
+  else {
+  	let next = current.next; 
+  	while (next) {
+  		if (next.val === val) {
+  			current.next = next.next;
+  			return;  
+  		}
+  		next = next.next; 
+  		current = current.next; 
+  	}
+  }
 };
 
 module.exports = LinkedList;
