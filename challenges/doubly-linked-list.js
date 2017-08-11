8f54e823e204194b94aa017ace3f969e1c6f8c89
Justino Mora <jmora258@gmail.com>
2017-08-11 09:26:57 -0700

    Refactor and fix bugs: doubly linked list challenge.

--- challenges/doubly-linked-list.js
+++ challenges/doubly-linked-list.js
@@ -16,54 +16,57 @@ function Node(val) {
 /*
 Adds a node to the end of the list
  */
-LinkedList.prototype.add = function(val) {
-  const node = new Node(val); 
+LinkedList.prototype.add = function (val) {
+  const node = new Node(val);
   // Check if empty or one node 
-  if (this.head === null)  this.head = this.tail = node; 
+  if (this.head === null) this.head = this.tail = node;
 
   if (this.tail !== null) {
-    this.tail.next = node; 
-    node.prev = this.tail; 
-    this.tail = node; 
+    this.tail.next = node;
+    node.prev = this.tail;
+    this.tail = node;
   }
 };
 
 /*
 Removes the first node with the inputted value
  */
-LinkedList.prototype.remove = function(val) {
-    // traverse through Doubly Linked List to find the value: 
+LinkedList.prototype.remove = function (val) {
+  // traverse through Doubly Linked List to find the value: 
 
-    let traverse = this.head; 
+  let traverse = this.head;
 
-    while (traverse !== null) {
-
-      // consider edge cases at the head and tail
-      if (traverse.val === val && traverse === this.head) {
-        this.head = this.head.next; 
-        traverse.next = null; 
-        this.head.prev = null; 
-        traverse = this.head; 
+  // Iterate throught entire doubly linked list 
+  while (traverse) {
+    if (traverse.value === val) {
+      // edge condition: value is at head 
+      if (traverse === this.head) {
+        this.head = this.head.next;
+        traverse.next = null;
+        this.head.prev = null;
+        traverse = this.head;
+        return;
       }
 
-      // tail: 
-      if ( traverse.val === val && traverse === this.tail ) {
-        this.tail = this.tail.prev; 
-        traverse.prev = null 
-        this.tail.next = null 
-        // done traversing 
+      // edge condition: value is at tail 
+      if (traverse === this.tail) {
+        this.tail = this.tail.prev;
+        traverse.prev = null
+        this.tail.next = null
+        return; // done traversing 
+
       }
 
-      // value found somewhere in the middle. 
-      if (traverse.val === val) {
-        traverse.prev.next = traverse.next; 
-        traverse.next.prev = traverse.prev; 
-        // Done traversing, set traverse prev & next values equal to null 
-        // for garbage colleciton
-        traverse.next = traverse.prev = null; 
-      }
-      traverse = traverse.next; 
+      // found node somewhere in the middle 
+      traverse.prev.next = traverse.next;
+      traverse.next.prev = traverse.prev;
+      // Done traversing, set traverse prev & next values equal to null 
+      // for garbage colleciton
+      traverse.next = traverse.prev = null;
+      return;
     }
+    traverse = traverse.next;
+  }
 };
 
 module.exports = LinkedList;
