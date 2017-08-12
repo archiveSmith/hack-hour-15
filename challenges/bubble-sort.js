0c0a6ca571f8ced1a7e911604fca9dc69a777d11
Melissa Schwartz <melissjs@gmail.com>
2017-08-12 09:10:58 -0700

    works

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -5,7 +5,20 @@
 
 
 function bubbleSort(array) {
-
+  const ret = array;
+  let swapped
+  do {
+    swapped = false
+    for (let i = 1; i < ret.length; ++i) {
+      if (ret[i - 1] > ret[i]) {
+        [ret[i], ret[i - 1]] = [ret[i - 1], ret[i]]
+        swapped = true
+      }
+    }
+  } while (swapped)
+  return ret
 }
 
 module.exports = bubbleSort;
+
+console.log(bubbleSort([9,2,14,3,2,7,8]))
