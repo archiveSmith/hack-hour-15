53672f851f06ea3f3b7991aaa8c8c2f3d6e8977d
Ian Ramos <i.am.ian.ramos@gmail.com>
2017-08-12 09:30:12 -0700

    finished bubble sort

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -5,7 +5,26 @@
 
 
 function bubbleSort(array) {
-
+    if(!Array.isArray(array)) return "Invalid argument"
+    let temp;
+    let i;
+    let sortedArr = array.slice()
+    let marker = 0
+    function recursive(sortedArr, marker) {
+        for(i=0; i<sortedArr.length-1; i++) {
+            if(sortedArr[i+1] < sortedArr[i]) {
+                temp = sortedArr[i]
+                sortedArr[i] = sortedArr[i+1]
+                sortedArr[i+1] = temp
+                marker ++
+            }
+            if(marker === 0 && i === sortedArr.length-2) return sortedArr
+        }
+        marker = 0;
+        return recursive(sortedArr, marker)
+    }
+    return recursive(sortedArr, marker)
 }
+
 
 module.exports = bubbleSort;
