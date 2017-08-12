28ac6e7c387d23964abf780b4fd259a858a34168
Christopher Dobler <chrisdobler@gmail.com>
2017-08-12 09:47:24 -0700

    Adds Bubble Sort

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -5,7 +5,19 @@
 
 
 function bubbleSort(array) {
-
+  if (!Array.isArray(array)) return 'Invalid Input';
+  if (array.length < 2) return array;
+  let sorted = false;
+  while (sorted === false) {
+    sorted = true
+    for (let i = 0; i < array.length - 1; i += 1) {
+      if (array[i] > array[i + 1]) {
+        [array[i], array[i + 1]] = [array[i + 1], array[i]];
+        sorted = false;
+      }
+    }
+  }
+  return array;
 }
 
 module.exports = bubbleSort;
