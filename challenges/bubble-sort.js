12fb5c5c5b94f1a8594f4bc9dcf4164ac0e3d821
Brian Schmalz <bschmalz@Brians-MacBook-Air.local>
2017-08-12 09:26:28 -0700

    Bubble sort.

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -5,7 +5,20 @@
 
 
 function bubbleSort(array) {
-
+ if (!Array.isArray(array) || array.length === 0) return false; 
+ if (array.length === 1) return array; 
+ let sorting = true;
+ let temp;  
+ while (sorting) {
+   sorting = false; 
+   for (let i = 0; i < array.length - 1; i++) {
+     if (array[i] > array[i + 1]) {
+     	sorting = true;
+     	[array[i], array[i+1]] = [array[i+1], array[i]];
+     }
+   }
+ }
+ return array; 
 }
 
 module.exports = bubbleSort;
