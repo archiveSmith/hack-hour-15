0110943d42345ef8eca269ae3d7bf5f0f863620c
mm0nr0e <monroe.mirand@gmail.com>
2017-08-12 09:14:04 -0700

    hackhour

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -5,7 +5,24 @@
 
 
 function bubbleSort(array) {
+  // set a flag to indicate if all pairs have been sorted
+  // if flag doesn't get set to false by end of loop, ur good
+  let flag = false;
 
+  while (!flag) {
+    flag = true; 
+    // largest # in unsorted array will always move to the very end, 
+    // so you don't have to check its index on subsequent passes
+    for(let i = 0; i < array.length - i - 1; i += 1) {
+      if(array[i] > array[i+1]) {
+        flag = false;
+        array[i] = array[i] ^ array[i+1];
+        array[i+1] = array[i] ^ array[i+1];
+        array[i] = array[i] ^ array[i+1];
+      }
+    }
+  }
+  return array;
 }
 
 module.exports = bubbleSort;
