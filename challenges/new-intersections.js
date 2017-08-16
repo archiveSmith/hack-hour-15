18f8cc699fa6a9df794356cc75296911a945eb7b
skykeeper0 <trungdinh0429@gmail.com>
2017-08-16 13:06:13 -0700

    Done

--- challenges/new-intersections.js
+++ challenges/new-intersections.js
@@ -17,8 +17,46 @@
  * 	 
  */
 
-function newIntersections(x, y){
 
+
+function newIntersections(x, y){
+  let lines = [];
+  for(let l1 = 0;l1 < x.length;l1++){
+    for(l2 = 0;l2<x.length;l2++){
+      if(makeLine(x[l1],x[l2],y[l1],y[l2])){
+        lines.push(makeLine(x[l1],x[l2],y[l1],y[l2]))
+      }
+    }
+  }
+  let points = []
+  for(let c = 0;c<lines.length;c++){
+    for(let z =0;z<lines.length;z++){
+      points.push(intersection(lines[c],lines[z]))
+    }
+  }
+  return points
 }
+function makeLine(x1,x2,y1,y2){
+  let gradient = (y2-y1)/(x2-x1)
+  let intersection = y1-gradient*x1
+  if(isNaN(gradient) || isNaN(intersection)){
+   return false 
+  }
+  return [gradient,intersection] 
+}
+
+function intersection(l1,l2){
+  let pointx = (l2[1] - l1[0])/l1[0]-l2[0]
+  let pointy = l1[0]*pointx + l1[1]
+  if(isNaN(pointx) || isNaN(pointy)){
+   return false 
+  }
+  return [pointx,pointy]  
+}
+
+let arr = [1,2,3,7,5]
+let zrr = [1,2,3,4,5]
+
+console.log(newIntersections(arr,zrr))
 
 module.exports = newIntersections;
