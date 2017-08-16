f805fdc8abb82e8d74ceaf7e879b1424eeb54383
Ian Ramos <i.am.ian.ramos@gmail.com>
2017-08-16 09:47:56 -0700

    did not finish new intersection

--- challenges/new-intersections.js
+++ challenges/new-intersections.js
@@ -9,7 +9,8 @@
  * such that there are OLD points above, below, to the left, and to the right of the NEW point
  *
  *   - 'OLD directly above NEW' means the NEW x-coordinate = OLD x-coordinate & NEW y-coordinate < OLD y-coordinate
- *
+ *     
+ *        
  * Constraints and Notes:
  *
  * 	 - x and y will contain the same number of elements
@@ -18,7 +19,25 @@
  */
 
 function newIntersections(x, y){
-
+    //x is an array of old points => [1,2,2,3] 
+    //y is an array of old points => [2,1,3,2]
+                                //=> [1,2],[2,1],[2,3],[3,2]
+                                //   1  2  3   
+                                // 1 X  *  X
+                                // 2 *  X  *
+                                // 3 X  *  X
+    // OLD x above NEW x => New x = [x[i], y[i-1]]    
+    // NEW x above OLD x => New x = [x[i], y[i+1]]                                                    
+    let container = []
+    let i;
+    let j;
+    for (i=0; i<x.length; i++){
+        
+        for (j=i+1; j<x.length-1; i++){
+           
+        }
+    }
+    
 }
 
 module.exports = newIntersections;
