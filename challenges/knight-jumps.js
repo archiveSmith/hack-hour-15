be87e8ea8fe3f30067056b197c7c5d965155a46e
Brett Beekley <brettbeekley@gmail.com>
2017-08-17 09:29:17 -0700

    Feels a little like cheating

--- challenges/knight-jumps.js
+++ challenges/knight-jumps.js
@@ -10,8 +10,29 @@
 //  example input:
 // var str = "(4 5)"
 
-function knightjumps(str) {
+// input
+  // A string, representing the coordinates of a knight on an 8x8 chess board in the format '(x y)'
+// output
+  // An integer, representing the number of moves the knight can take if there are no other pieces on the board
 
+function knightjumps(str) {
+  // Parse the input string into X and Y
+  const x = parseInt(str[1]);
+  const y  = parseInt(str[3]);
+  // Find the minimum X and Y distances from a board edge
+  const x_min = Math.min(x-1, 8-x);
+  const y_min = Math.min(y-1, 8-y);
+  // Hmmm
+  const sum = x_min + y_min;
+  if (sum >= 4) return 8;
+  if (sum == 3) return 6;
+  if (sum == 2) return 4;
+  if (sum == 1) return 3;
+  if (sum == 0) return 2;
 }
+
+// console.log(knightjumps('(4 5)'), 8);
+// console.log(knightjumps('(1 5)'), 4);
+// console.log(knightjumps('(1 8)'), 2);
 
 module.exports = knightjumps;
