7df29fc850ef960ac9a3e87a24ff24c7ea687999
icrustandi <icrustandi@gmail.com>
2017-08-18 09:37:05 -0700

    pascal's triangle

--- challenges/pascal-triangle.js
+++ challenges/pascal-triangle.js
@@ -33,7 +33,29 @@
 */
 
 function pascalTriangle(numRows) {
+  const triangle = [];
+  function stack(numRows, row = 1) {
+    if (row === 1) triangle.push([1]);
+    else if (row === 2) triangle.push([1, 1]);
+    else {
+      let newRow = [];
+      for (let i = 0; i < row; i += 1) {
+        if (i === 0 || i === row - 1) newRow.push(1);
+        else {
+          newRow.push(triangle[triangle.length - 1][i -1] + triangle[triangle.length - 1][i ]);
+        }
+      }
+      
+      triangle.push(newRow);
+    }
+    if (row === numRows) return triangle;
+    stack(numRows, row += 1);
+  }
 
+  stack(numRows);
+  return triangle;
 }
+
+console.log(pascalTriangle(7))
 
 module.exports = pascalTriangle;
