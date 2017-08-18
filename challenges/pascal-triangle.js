c1b0009cc5e2ede1cb103bef90087598b8384811
Brian Schmalz <bschmalz@Brians-MacBook-Air.local>
2017-08-18 09:40:38 -0700

    Pascal fail.

--- challenges/pascal-triangle.js
+++ challenges/pascal-triangle.js
@@ -33,7 +33,21 @@
 */
 
 function pascalTriangle(numRows) {
-
+  if (numRows === 1) return [[1]];
+  if (numRows === 2) return [[1], [1,1]]; 
+  let result = [[1], [1,1]]; 
+  for (let i = 2; i <= numRows; i++) {
+    let current = [1];
+    for (let y = 1; y <= Math.floor(i / 2); y++) {
+      current.push(result[i-1][y-1] + result[i-1][y]);
+    }
+    let secondhalf; 
+    if (i % 2 === 0) secondhalf = current.slice().reverse(); 
+    else secondhalf = current.slice(0, -1).reverse(); 
+    let final = current.concat(secondhalf);
+    result.push(final); 
+  }
+  return result; 
 }
 
 module.exports = pascalTriangle;
