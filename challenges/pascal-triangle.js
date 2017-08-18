3f463c2b27669f0792542328c21f6f989b359172
Ian Ramos <i.am.ian.ramos@gmail.com>
2017-08-18 09:43:53 -0700

    finished pascal-triangle

--- challenges/pascal-triangle.js
+++ challenges/pascal-triangle.js
@@ -32,8 +32,45 @@
   ]
 */
 
-function pascalTriangle(numRows) {
+//beginning and end of array are always 1
+//numRows corresponds to how many arrays there are
+//level and length starts at 1 and increases until it hits numRows
 
+function pascalTriangle(numRows) {
+  
+  if (typeof numRows !== 'number') return 'Invalid argument'
+
+  if (numRows === 0) return new Array()
+  if (numRows === 1) return [[1]]
+
+  let i;
+  let tempLevel = [1,1];
+  const container = [[1], [1,1]]
+  if (numRows === 2) return container
+
+  let newLevel;
+  for (i=3; i<numRows+1; i++) {
+    newLevel = new Array(i)
+    newLevel[0] = 1
+    newLevel[newLevel.length-1] = 1
+    
+    helper(newLevel, tempLevel)
+    container.push(newLevel)
+
+    tempLevel = newLevel
+  }
+  return container;
 }
+
+const helper = (newLevel, tempLevel) => {
+  let index = newLevel.findIndex((el) => el === undefined)
+  if (index === -1) return newLevel;
+
+  newLevel[index] = tempLevel[index-1] + tempLevel[index]
+
+  helper(newLevel, tempLevel) 
+}
+
+
 
 module.exports = pascalTriangle;
