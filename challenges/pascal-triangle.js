2760fb4f2fbf2d50bf4f2eabfa99580ee40c198a
Justino Mora <jmora258@gmail.com>
2017-08-19 17:39:06 -0700

    Complete pascal triangle challenge.

--- challenges/pascal-triangle.js
+++ challenges/pascal-triangle.js
@@ -32,8 +32,43 @@
   ]
 */
 
-function pascalTriangle(numRows) {
 
-}
+function pascalTriangle(numRows) {
+  // Base Case: no rows
+  if (numRows === 0) return []; 
+
+  const triangle = [[1]]; 
+  // Outer Loop: loop through each Row
+  for( let i = 0; i < numRows - 1; i += 1 ) {
+    const subArr = [1]; 
+
+    const prevRow = triangle[triangle.length - 1]; 
+    // [[1] [1]]
+    for (let j = 0; j < prevRow.length - 1; j += 1) {
+      subArr.push(prevRow[j + 1] + prevRow[j]); 
+    }
+    subArr.push(1); 
+    triangle.push(subArr); 
+  }
+  return triangle; 
+} 
+
+
+// // Possibly not the right implementation, refactor this(below) above this line: 
+// function pascalTriangle(numRows) {
+//   let triangle = []; 
+//   // nested for loop to solve this challenge 
+//   // OUTER: from 0 to numRows 
+//   for (let i = 0 ; i < numRows ; i += 1) { 
+//     let subArr = []; 
+//     // INNER: loop through 
+//     for (let j = 0; j < i + 1; j += 1 ) {
+//       if (j === 0 || j === i) subArr[j].push(1); 
+//       // Ignore: subArr.push( resultArr[i-1][j] + resultArr[i-1][j+1]);
+//     }
+//     triangle.push(subArr); 
+//   }
+//   return triangle; 
+// }
 
 module.exports = pascalTriangle;
