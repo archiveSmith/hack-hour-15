ea2f13e6d9f189cbcbb55c3236328c59b8210687
Ian Ramos <i.am.ian.ramos@gmail.com>
2017-08-21 09:46:36 -0700

    did not finish poker

--- challenges/poker.js
+++ challenges/poker.js
@@ -20,7 +20,62 @@
  * BONUS2: Create a deck of cards function that generates two random hands for you.
  */
 function poker(hand1, hand2) {
+    let sortedHand1 = hand1.sort((a,b)=>{return a-b})
 
+    let sortedHand2 = hand2.sort((a,b)=>{return a-b})
+
+    let resultHand1 = findHand(sortedHand1)
+
+    let resultHand2 = findHand(sortedHand2)
+
+    switch (key) {
+        case value:
+            
+            break;
+    
+        default:
+            break;
+    }
 }
 
+
+const findHand = (hand) => {
+    let result;
+    let temp = hand[0];
+    let container = []
+    let counterCard = 1
+    let counterSwitch = 0
+    let i;
+
+    for(i=1; i<hand.length; i++) {
+        if (temp === hand[i]) counterCard ++
+        else {
+            temp === hand[i];
+            container.push(counterCard)
+            counterCard = 1
+            counterSwitch ++
+        }
+    }
+
+    let determinePairs = container.filter( (el) => el !== 2)
+
+    if(container[0] === 4 || 1 && counterSwitch === 2) result = '4ofakind'
+
+    if(container[0] === 3 || 2 && counterSwitch === 2) result = 'fullhouse'      
+
+    if(counterSwitch === 4) result = 'straight'
+
+    if(ontainer.includes(3) && counterSwitch === 3) result = '3ofakind'
+        
+    if(determinePairs.length === 1 && counterSwitch === 3) result = '2ofakind'   
+
+    if(determinePairs.length === 3  && counterSwitch === 3) result = '1ofakind' 
+
+    if(counterSwitch === 4 && ) result = 'highcard'
+
+    return result
+}
+
+
+// * 4-of-a-kind > full house > straight > 3-of-a-kind > 2-pair > 1-pair > high card
 module.exports = poker;
