80254e9bc9b33f0627bbaf5e0f0e4c547b91800c
Brett Beekley <brettbeekley@gmail.com>
2017-08-21 09:46:05 -0700

    Not working for straight

--- challenges/poker.js
+++ challenges/poker.js
@@ -15,12 +15,53 @@
  *
  * Clearly real-world poker has more complex rules for tied ranks, but we want you
  * to focus on overall architecture rather than edge cases. Have fun!
- * 
+ *
  * BONUS: Account for suits and add in Flush & Straight Flush/Royal Flush.
  * BONUS2: Create a deck of cards function that generates two random hands for you.
  */
-function poker(hand1, hand2) {
 
+// Input
+  // Two arrays of five integers 2 to 14
+// Output
+  // String "Player ${x} wins", where x is the player with the better hand1
+
+function poker(hand1, hand2) {
+  // Get each hand's rank
+  const rank1 = rankHand(hand1);
+  const rank2 = rankHand(hand2);
+  // If they're the same
+  if (rank1[0] === rank2[0]) {
+    // Return the winner
+    if (rank1[1] > rank2[1]) return "Player 1 wins";
+    else if (rank1[1] < rank2[1]) return "Player 2 wins";
+    else return "Draw";
+  }
+  // Else return winner
+  else if (rank1[0] > rank2[0]) return "Player 1 wins";
+  else return "Player 2 wins";
 }
+
+// Takes in a hand and returns an array [rank, highCard]
+const rankHand = hand => {
+  const store = {};
+  // Iterate through the hand
+    // Increment count for that card
+  hand.forEach(card => store[card] ? store[card] += 1 : store[card] = 1);
+  // Reduce stored cards to high card
+  const highCard = Math.max(...Object.keys(store));
+  // Reduce stored cards to first highest and second highest X of a kind
+  const pairs = Object.values(store).reduce((pairs, card) => {
+    if (card > pairs.highest) [pairs.highest, pairs.second] = [card, pairs.highest];
+    else if (card > pairs.second) pairs.second = card;
+    return pairs;
+  }, {highest: 0, second: 0});
+  // Calculate rank
+  const rank = 3*pairs.highest + pairs.second;
+  return[rank,highCard]
+}
+
+// test
+// console.log(poker([3,5,5,5,2], [4,6,7,8,8]), "Player 1 wins");
+// console.log(poker([8, 8, 8, 4, 5], [4, 4, 4, 2, 14]), "Player 2 wins");
 
 module.exports = poker;
