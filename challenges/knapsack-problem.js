fc7273ae52191a2b9fa8a77c0481a1e0edb3c94d
Brett Beekley <brettbeekley@gmail.com>
2017-08-25 09:39:36 -0700

    Not quite working

--- challenges/knapsack-problem.js
+++ challenges/knapsack-problem.js
@@ -9,8 +9,36 @@
   solveKnapsack(items, 5); // returns 9 (from items[1] and items[2])
 */
 
-function solveKnapsack(items, weightAvailable) {
+// Input
+  // An array, items, of objects with properties:
+    // weight: integer
+    // value: integer
+  // An integer, representing the maxumum weight to carry
+// Output
+  // An integer, representing the maximum sum of values of items that have a sum of weights under the weightAvailable
 
+function solveKnapsack(items, weightAvailable) {
+  // Map and sort the items by a ratio of weights to values
+  sortedItems = items
+    .map(item => Object.assign(item, {ratio: item.value / item.weight}))
+    .sort((a,b) => a.ratio - b.ratio);
+
+  // Track the sum of values and weights
+  let values = 0;
+  let weights = 0;
+  // Iterate through sorted items
+  for (let curItem = sortedItems.pop(); weights + curItem.weight <= weightAvailable; curItem = sortedItems.pop()) {
+    // Increment values and weights
+    values += curItem.value;
+    weights += curItem.weight;
+  }
+  // Return the sum of values
+  return values;
 };
+
+// test
+items = [{weight: 1, value : 3}, {weight: 2, value : 4}, {weight: 3, value : 5}];
+console.log(solveKnapsack(items, 3), 7); // returns 7 (from items[0] and items[1])
+console.log(solveKnapsack(items, 5), 9); // returns 9 (from items[1] and items[2])
 
 module.exports = solveKnapsack;
