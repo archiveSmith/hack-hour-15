80d507669dcf9d7c5d6f159a6c7134696cc4fb93
Chris Amori <chris.amori93@gmail.com>
2017-09-02 20:16:57 -0700

    Complete anagrams

--- challenges/anagrams.js
+++ challenges/anagrams.js
@@ -7,13 +7,61 @@
  */
 
 /**
-  * example:
-  * var result = anagrams('abc');
-  * console.log(result); // [ 'abc', 'acb', 'bac', 'bca', 'cab', 'cba' ]
-  */
+ * example:
+ * var result = anagrams('abc');
+ * console.log(result); // [ 'abc', 'acb', 'bac', 'bca', 'cab', 'cba' ]
+ */
+
+// function anagrams(string) {
+//   if (string.length === 2) return [string, string.charAt(1) + string.charAt(0)];
+
+//   const output = [];
+
+//   for (let i = 0; i < string.length; i += 1) {
+//     let currLetter = string.charAt(i);
+//     let anagramsWithoutCurrLetter = anagrams(string.slice(0, i) + string.slice(i + 1));
+
+//     for (let j = 0; j < anagramsWithoutCurrLetter.length; j += 1) {
+//       output.push(currLetter + anagramsWithoutCurrLetter[j]);
+//     }
+//   }
+//   // Return array of all possible anagrams      
+//   return output;
+// }
+
+// console.log(anagrams('hey'));
+
+// module.exports = anagrams;
+
+
+
+
+/**
+ * Given a single input string, write a function that produces all possible anagrams
+ * of a string and outputs them as an array. At first, don't worry about
+ * repeated strings.  What time complexity is your solution?
+ *
+ * Extra credit: Deduplicate your return array without using uniq().
+ */
+
+/**
+ * example:
+ * var result = anagrams('abc');
+ * console.log(result); // [ 'abc', 'acb', 'bac', 'bca', 'cab', 'cba' ]
+ */
 
 function anagrams(string) {
+  if (string.length === 1) return [string];
 
+  const output = [];
+
+  for (let i = 0; i < string.length; i += 1) {
+    let currLetter = string.charAt(i);
+    let slicedString = string.slice(0, i) + string.slice(i + 1);
+    output.push(...anagrams(slicedString).map(anagram => currLetter + anagram));
+  }
+
+  return output;
 }
 
-module.exports = anagrams;
+console.log(anagrams('hey'));
