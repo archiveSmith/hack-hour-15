f4222b4b63c045b497fe5942f0117286bf7120c2
Chris Amori <chris.amori93@gmail.com>
2017-09-02 21:28:31 -0700

    Complete delete-dups

--- challenges/delete-dups.js
+++ challenges/delete-dups.js
@@ -9,11 +9,15 @@
  * Extra:
  * How would you solve this problem if a temporary buffer is not allowed?
  */
-
-
-
 function deleteDups(head) {
+  let foundValues = {};
+  let currentNode = head;
 
+  while(currentNode.next) {
+    foundValues[currentNode.value] = true;
+    if (foundValues[currentNode.next.value] === true) currentNode.next = currentNode.next.next;
+    currentNode = currentNode.next;
+  }
 }
 
 module.exports = deleteDups;
